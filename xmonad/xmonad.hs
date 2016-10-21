84d7c4a742cf1cc0a4c6a921f56ea63413ffe110
yashamon <yasha.savelyev@gmail.com>
2016-10-21 18:57:03 -0500

    -a

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,14 +7,16 @@ import XMonad.Layout.NoBorders
 import XMonad.Hooks.UrgencyHook
 import XMonad.Util.EZConfig(additionalKeysP, additionalKeys, removeMouseBindings)
 myWorkspaces :: [String]
-myWorkspaces = [ "shell", "web", "media", "torrent", "shell-misc", "6", "7", "8"
+myWorkspaces = [ "shell", "zathura", "chrome", "media", "shell-misc", "torrent", "7", "8"
                , "mail" ]
 
 -- layoutHook = avoidStruts $ smartBorders (tall ||| Full)
 mylayoutHook = avoidStruts $ smartBorders (tall ||| Full)
                    where  tall = Tall 1 (3/100) (1/2)
 myManageHook = composeAll
-   [ className =? "Chromium" --> doShift "web"
+   [ 
+     className =? "Zathura" --> doShift "zathura"
+   , className =? "Chromium" --> doShift "web"
    , className =? "Transmission-gtk" --> doShift "torrent"
    , className =? "Steam" --> doShift "media"
    , className =? "Vlc" --> doShift "media"
