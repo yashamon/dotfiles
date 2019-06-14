20c07b68b057968416c16af2ad8deb812de92e9b
yashamon <yasha.savelyev@gmail.com>
2019-06-14 19:25:30 +0000

    -a

--- makesymlinksChromeos.sh
+++ makesymlinksChromeos.sh
@@ -54,7 +54,7 @@ mkdir ~/.config/nvim/autoload
 ln -s ~/dotfiles/vim-plug/plug.vim ~/.config/nvim/autoload/plug.vim
 
 
-# zathura
+# zathura, termitie
 mkdir ~/.config/zathura
 mkdir ~/.config/termite
 ln -s ~/dotfiles/zathurarc  ~/.config/zathura/zathurarc
