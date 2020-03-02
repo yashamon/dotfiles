f29ad14dd9644e956e3eab437b940d86d4c907a8
yashamon <yasha.savelyev@gmail.com>
2020-03-02 20:04:03 +0000

    -a

--- makesymlinksChromeos.sh
+++ makesymlinksChromeos.sh
@@ -24,7 +24,7 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 # mkdir ~/.vifm
-mkdir ~/.xmonad
+# mkdir ~/.xmonad
 # mkdir ~/.config
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
