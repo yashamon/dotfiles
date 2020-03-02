b94116d7a46c579b09ff18c00d1e99f5913bcff9
yashamon <yasha.savelyev@gmail.com>
2020-03-02 20:02:42 +0000

    -a

--- makesymlinksChromeos.sh
+++ makesymlinksChromeos.sh
@@ -37,7 +37,7 @@ done
     echo "Moving vifm"
 mkdir ~/bin
 cp ~/dotfiles/bin/vf ~/bin/vf
-mv ~/.config ~/dotfiles_old asdf
+mv ~/.config ~/dotfiles_old 
 mkdir ~/.config
 echo "link config"
 mv  ~/dotfiles/config ~/dotfiles_old
