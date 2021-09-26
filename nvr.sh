2851229f3859507c7e443605d21e7317973771d1
yashamon <yasha.savelyev@gmail.com>
2021-09-26 10:57:09 -0500

    -a

--- nvr.sh
+++ nvr.sh
@@ -1,5 +1,5 @@
  #! /bin/sh 
 
-var =$(cat ~/servername.txt)   
-echo var
-~/.local/bin/nvr --servername var --remote-silent
+var=$(cat ~/servername.txt)   
+echo $var
+# ~/.local/bin/nvr --servername var --remote-silent
