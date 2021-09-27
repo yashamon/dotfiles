9906f57868533dcae2759a1afbcdfa07ce4e9c3d
yashamon <yasha.savelyev@gmail.com>
2021-09-27 17:40:36 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -1,6 +1,4 @@
 #! /bin/sh 
-touch more
-touch sentence_${1}
 cat $1 | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|    
  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' > more_${1}
 sentence2.sh more_${1}
