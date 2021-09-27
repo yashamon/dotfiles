e26b9e2a7ef9aa34f7f2f7c1356949a0241e5401
yashamon <yasha.savelyev@gmail.com>
2021-09-27 17:42:14 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -1,7 +1,7 @@
 #! /bin/sh 
 cat $1 | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|    
  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' > more_${1}
-sentence2.sh more_${1}
-
+sentence2.sh more_${1} 
+rm more
 #sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\'$'\n''|g' more > sentence_${1} 
 #cat sentence_${1} 
