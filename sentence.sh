daebb1d78d8d6b04732e7893da69028d00142882
yashamon <yasha.savelyev@gmail.com>
2021-09-27 11:04:33 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -2,5 +2,5 @@
 touch more
 cat $1 | tr '\n' ' ' > more 
 touch sentence_${1}
-sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\''\n''|g' more > sentence_${1} 
+sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\'$'\n''|g' more > sentence_${1} 
 cat sentence_${1} 
