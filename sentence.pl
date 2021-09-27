5b511688d86818b3b10b84e6824ef8d8e725adb2
yashamon <yasha.savelyev@gmail.com>
2021-09-27 10:07:28 -0500

    -a

--- sentence.pl
+++ sentence.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/perl
 use warnings;
 
-perl -MLingua::EN::Sentence=get_sentences -00 -e '
-$t = <>;         # slurp the whole file
-$t =~ tr{\n}{ }; # convert newlines to spaces
-print "$_\n" for grep { /graph/ } @{get_sentences($t)}' dijk
+
 
 
