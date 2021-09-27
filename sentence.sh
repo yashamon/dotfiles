7ede87c76f4c58277d059c4967b5dd02e39bbe1e
yashamon <yasha.savelyev@gmail.com>
2021-09-27 11:50:56 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -3,7 +3,7 @@ touch more
 touch sentence_${1}
 cat $1 | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|   
  tr '\^' ' ' | tr '\_' ' '  > more 
-sentence2.sh more > sentence_${1}
+sentence2.sh more 
 
 #sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\'$'\n''|g' more > sentence_${1} 
 #cat sentence_${1} 
--- sentence2.sh
+++ sentence2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-touch more2 
+touch sentence_${1} 
 perl -MLingua::EN::Sentence=get_sentences -00 -e '
 $t = <>;         # slurp the whole file
 $t =~ tr{\n}{ }; # convert newlines to spaces
