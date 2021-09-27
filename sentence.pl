cfcd79023410a858cbeb39ba8b139c47fbfc97d0
yashamon <yasha.savelyev@gmail.com>
2021-09-27 11:08:05 -0500

    -a

--- sentence.pl
+++ sentence.pl
@@ -1,1 +1,5 @@
 
+perl -MLingua::EN::Sentence=get_sentences -00 -e '
+$t = <>;         # slurp the whole file
+$t =~ tr{\n}{ }; # convert newlines to spaces
+print "$_\n" for grep { /graph/ } @{get_sentences($t)}' $1 
