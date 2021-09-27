e4c41d582d3f4b01f720bf772abd09d1c5417eab
yashamon <yasha.savelyev@gmail.com>
2021-09-27 11:48:28 -0500

    -a

--- sentence2.sh
+++ sentence2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-touch sentence_${1}
+touch more2 
 perl -MLingua::EN::Sentence=get_sentences -00 -e '
 $t = <>;         # slurp the whole file
 $t =~ tr{\n}{ }; # convert newlines to spaces
