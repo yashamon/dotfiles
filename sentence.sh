722f0feb005c91c7a9743adbc5e45d65873c6d9e
yashamon <yasha.savelyev@gmail.com>
2021-09-27 17:52:28 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -1,7 +1,7 @@
 #! /bin/sh 
 cat $1 | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|    
  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' > more_${1}
-sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\'$'\n''|g' more_${1} > sentence_${1} more_${1} 
+sed 's|\.[[:blank:]]*|&\'$'\n''|g' more_${1} > sentence_${1} more_${1} 
 rm more_${1}
 #sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\'$'\n''|g' more > sentence_${1} 
 #cat sentence_${1}  
