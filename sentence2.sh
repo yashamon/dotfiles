c13866e9f7c74f0e996fbd969aea1a1b6b0c743a
yashamon <yasha.savelyev@gmail.com>
2021-09-27 11:30:00 -0500

    -a

--- sentence2.sh
+++ sentence2.sh
@@ -3,5 +3,5 @@ touch sentence_${1}
 perl -MLingua::EN::Sentence=get_sentences -00 -e '
 $t = <>;         # slurp the whole file
 $t =~ tr{\n}{ }; # convert newlines to spaces
-print "$_\n" for grep { // } @{get_sentences($t)}' > sentence_${1}  
+print "$_\n" for grep { // } @{get_sentences($t)}' $1 > sentence_${1}  
 exit
