fb11fb42945949d09c148d9a323adf26bc7b1c6f
yashamon <yasha.savelyev@gmail.com>
2021-09-28 15:56:05 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -5,9 +5,12 @@
 #awk '{print ln++  ":  "  $0 }' $1 | sed 's|\.[[:blank:]]*|[[:blank:]]N|g' |
    #tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | sed 's|\.[[:blank:]]*|&\n''|g' more_${1} > sentence_${1} 
 
-#awk '{print ln++  ":  "  $0 }' $1 |  tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' '| sed 's|\.[[:blank:]]*|&\n''|g' > sentence_${1} 
+#awk '{print ln++  ":  "  $0 }' $1 |  tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' '| sed 's|\.[[:blank:]]*|&\n''|g' > sentence_${1}  
 
-awk '{print ln++  ":  "  $0 }' kan.tex | sed 's|s\.t\.|''|g' | sed 's|i\.e\.|''|g' |  awk -v RS= '{gsub(/\.[[:space:]]*\n/,"\.\@"NR+1": ",$0);print}' | awk '{gsub(/\.\s/,"&"NR": ",$0);print}' | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | sed 's|\.\s|&\n''|g'  | sed 's|\.\@|&\n''|g'> blah
+awk '{print NR ":  "  $0 }' kan.tex | awk -v RS= '{gsub(/\.[[:space:]]*\n/,".@\n",$0);print}'| awk '{gsub(/.[[:space:]]\{1,\}/,"&"NR":+ ",$0);print}' | sed 's|s\.t\.|''|g' | sed 's|i\.e\.|''|g' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'  > blah 
+
+
+awk 'if NF {print ln++  ":  "  $0 }' kan.tex | sed 's|s\.t\.|''|g' | sed 's|i\.e\.|''|g' |  awk -v RS= '{gsub(/\.[[:space:]]*\n/,".@"NR+1": ",$0);print}' | awk '{gsub(/\.\s/,"&"NR": ",$0);print}' | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | sed 's|\.\s|&\n''|g'  | sed 's|\.\@|&\n''|g'> blah 
 
 
 
