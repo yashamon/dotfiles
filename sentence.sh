a227757eb09002bb53ff2217963ab6046c0dec92
yashamon <yasha.savelyev@gmail.com>
2021-09-27 21:50:43 -0500

    -a

--- sentence.sh
+++ sentence.sh
@@ -1,10 +1,8 @@
 #! /bin/sh 
 
 
-awk '{print ln++  ":  "  $0 }' 
-$1 | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|    
- tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' > more_${1}
-sed 's|\.[[:blank:]]*|&\n''|g' more_${1} > sentence_${1} more_${1} 
+awk '{print ln++  ":  "  $0 }' $1 | tr '\n' ' ' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' > more_${1}
+sed 's|\.[[:blank:]]*|&\n''|g' more_${1} > sentence_${1} 
 rm more_${1}
 #sed 's|[[:alpha:]]\{3\}\.[[:blank:]]*|&\'$'\n''|g' more > sentence_${1} 
 #cat sentence_${1}  
