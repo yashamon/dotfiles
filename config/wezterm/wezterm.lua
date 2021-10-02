843d1464c2f1f6798bb6566dbe42388cfd202baa
yashamon <yasha.savelyev@gmail.com>
2021-10-01 19:32:34 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -117,7 +117,7 @@ end);
 
 {
   hide_tab_bar_if_only_one_tab = true,
-  font = wezterm.font("Fira Code"),
+  font = wezterm.font("Jet Brains Mono"),
   font_size = 20,
   --color_scheme = "Solarized Dark - Patched",
   --color_scheme = "The Hulk",
