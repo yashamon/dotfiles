703d56f40ba4d7599f28246c57c4e97ecfc07917
yashamon <yasha.savelyev@gmail.com>
2021-10-09 14:32:50 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -62,7 +62,7 @@ return {
  },
   hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font("Fira Code"),
-  font_size == 20,
+  font_size = 20,
   --color_scheme = "Solarized Dark - Patched",
   --color_scheme = "The Hulk",
   --color_scheme = "JetBrians Dracula",
@@ -74,7 +74,7 @@ return {
   keys = {
     -- This will create a new split and run your default program inside it 
     --
-      harfbuzz_features = {"zero"},
+harfbuzz_features = {"zero"},
   scrollback_lines = 100000,
 }
 
