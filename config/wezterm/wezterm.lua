7a6193830b851b4285dea1ba51c248c9242f4cc3
yashamon <yasha.savelyev@gmail.com>
2021-10-09 14:33:05 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -71,8 +71,7 @@ return {
   --color_scheme = "Cobalt Neon",
   color_scheme = "Dark Matrix",
   exit_behavior = "Close",
-  
-    -- This will create a new split and run your default program inside it 
+  -- This will create a new split and run your default program inside it 
     --
 harfbuzz_features = {"zero"},
 scrollback_lines = 100000,
