12978439b2f5b9f4efeb43bc7031b32500cf9f4b
yashamon <yasha.savelyev@gmail.com>
2021-10-09 14:49:59 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,51 +2,49 @@ local wezterm = require 'wezterm'
 local io = require 'io';
 local os = require 'os';
 
+
 wezterm.on("trigger-vim-with-scrollback", function(window, pane)
   -- Retrieve the current viewport's text.
   -- Pass an optional number of lines (eg: 2000) to retrieve
   -- that number of lines starting from the bottom of the viewport.
   local scrollback = pane:get_lines_as_text();
--- Create a temporary file to pass to vim
+
+  -- Create a temporary file to pass to vim
   local name = os.tmpname();
   local f = io.open(name, "w+");
   f:write(scrollback);
   f:flush();
   f:close();
+
   -- Open a new window running vim and tell it to open the file
-window:perform_action(wezterm.action{SpawnCommandInNewWindow={
+  window:perform_action(wezterm.action{SpawnCommandInNewWindow={
     args={"nvim", name}}
   }, pane)
-wezterm.sleep_ms(1000);
+
+  -- wait "enough" time for vim to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous
+  -- wrt. running this script and are not awaitable, so we just pick
+  -- a number.  We don't strictly need to remove this file, but it
+  -- is nice to avoid cluttering up the temporary file directory
+  -- location.
+  wezterm.sleep_ms(1000);
   os.remove(name);
 end)
 return {
-hide_tab_bar_if_only_one_tab = true,
-font = wezterm.font("Fira Code"),
-font_size = 20,
---color_scheme = "Solarized Dark - Patched",
---color_scheme = "The Hulk",
---color_scheme = "JetBrians Dracula",
---color_scheme = "Alien Blood",
---color_scheme = "Dracula",
---color_scheme = "Cobalt Neon",
-color_scheme = "Dark Matrix",
-exit_behavior = "Close",
-harfbuzz_features = {"zero"},
-scrollback_lines = 100000,
-keys = {
+
+  keys = {
     {key="V", mods="CTRL|SHIFT",
       action=wezterm.action{EmitEvent="trigger-vim-with-scrollback"}},
      {
       --key="%", mods="CTRL|SHIFT|ALT",
-      key="c", mods="CTRL|SHIFT",
+      key="c", mods="ALT|SHIFT",
       action=wezterm.action{
       action=wezterm.action{CloseCurrentPane={confirm=false}}
           }
       },
     {
       --key="%", mods="CTRL|SHIFT|ALT",
-      key="h", mods="CTRL|SHIFT",
+      key="h", mods="ALT|SHIFT",
       action=wezterm.action{
         SplitHorizontal={
           domain="CurrentPaneDomain"
@@ -55,7 +53,7 @@ keys = {
     },
     {
       --key="^", mods="CTRL|SHIFT|ALT",
-      key="v", mods="SHIFT|ALT",
+      key="v", mods="ALT|SHIFT",
       action=wezterm.action{
         SplitVertical={
           domain="CurrentPaneDomain"
@@ -64,3 +62,5 @@ keys = {
     },
   }
  }
+
+
