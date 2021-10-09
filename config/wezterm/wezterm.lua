4cced194e23d30663f8642b3e3e5e39dd4b26dcb
yashamon <yasha.savelyev@gmail.com>
2021-10-09 15:06:04 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -34,6 +34,9 @@ keys = {
     {key="V", mods="CTRL",
       action=wezterm.action{EmitEvent="trigger-vim-with-scrollback"}},
   },
+enable_tab_bar = false,
+  harfbuzz_features = {"zero"},
+  scrollback_lines = 100000,
 
   hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font("Fira Code"),
