9dd1e23fb2339e2551f1161939baacd5cc618df3
yashamon <yasha.savelyev@gmail.com>
2021-10-09 14:08:33 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -48,6 +48,7 @@ return {
   keys = {
     -- This will create a new split and run your default program inside it 
     --
+    
     {
       --key="%", mods="CTRL|SHIFT|ALT",
       key="h", mods="ALT-SHIFT",
