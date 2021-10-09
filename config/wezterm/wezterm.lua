0c64c3975d88050bb63fa9c9473c823df2bb3c32
yashamon <yasha.savelyev@gmail.com>
2021-10-09 14:34:08 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,16 +3,16 @@ local io = require 'io';
 local os = require 'os';
 
 hide_tab_bar_if_only_one_tab = true,
-  font = wezterm.font("Fira Code"),
-  font_size = 20,
-  --color_scheme = "Solarized Dark - Patched",
-  --color_scheme = "The Hulk",
-  --color_scheme = "JetBrians Dracula",
-  --color_scheme = "Alien Blood",
-  --color_scheme = "Dracula",
-  --color_scheme = "Cobalt Neon",
-  color_scheme = "Dark Matrix",
-  exit_behavior = "Close",
+font = wezterm.font("Fira Code"),
+font_size = 20,
+--color_scheme = "Solarized Dark - Patched",
+--color_scheme = "The Hulk",
+--color_scheme = "JetBrians Dracula",
+--color_scheme = "Alien Blood",
+--color_scheme = "Dracula",
+--color_scheme = "Cobalt Neon",
+color_scheme = "Dark Matrix",
+exit_behavior = "Close",
   -- This will create a new split and run your default program inside it 
     --
 harfbuzz_features = {"zero"},
