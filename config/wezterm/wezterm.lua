db7889993f8306c787ea93153e9e4aff8e910963
yashamon <yasha.savelyev@gmail.com>
2021-10-09 14:29:32 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -32,7 +32,7 @@ end)
 return {
   keys = {
     {key="V", mods="CTRL|SHIFT",
-      action=wezterm.action{EmitEvent="trigger-vim-with-scrollback"}},
+      action=wezterm.action{EmitEvent="trigger-vim-with-scrollback"}}, 2 = 
   },
   hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font("Fira Code"),
