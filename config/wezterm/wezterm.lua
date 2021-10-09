478d79f198c79e9b07e49adeb7347471a3e7c557
yashamon <yasha.savelyev@gmail.com>
2021-10-09 17:24:37 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -45,9 +45,8 @@ harfbuzz_features = {"zero"},
   -- color_scheme = "Dark Matrix",
   exit_behavior = "Close",
 keys = {
---
---     --
--- --key="e", mods="CTRL|ALT",
+
+key="e", mods="CTRL|ALT",
       action=wezterm.action{EmitEvent="trigger-vim-with-scrollback"}},
 
  {key="C", mods="ALT",
