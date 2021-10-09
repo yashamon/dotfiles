3389f35736f91b40bac877105c83233904236e7b
yashamon <yasha.savelyev@gmail.com>
2021-10-09 15:30:59 -0500

    -a

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -55,6 +55,7 @@ keys = {
      action=wezterm.action{CloseCurrentPane={confirm=false}}
   }
 },
+
  -- {key="%", mods="ALT",
  --      action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
    -- {
