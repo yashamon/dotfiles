7212027800595d1916d90777314d86be0fd637f9
yashamon <yasha.savelyev@gmail.com>
2021-11-17 20:05:33 -0600

    -a

--- config/xplr/init.lua
+++ config/xplr/init.lua
@@ -1,2 +1,6 @@
 package.path = os.getenv("HOME") .. '/.config/xplr/plugins/?/src/init.lua'
 
+require("material-landscape2").setup()
+
+-- The setup arguments might differ for different plugins.
+-- Visit the project README for setup instructions.
