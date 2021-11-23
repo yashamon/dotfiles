e10b46e716185845eaa564959a0442720e70cc87
yashamon <yasha.savelyev@gmail.com>
2021-11-22 21:22:28 -0600

    -a

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -68,7 +68,8 @@ xrandr -s 3440x1440
 "
 alias config="cd ~/dotfiles/; push; cd ~/workspacemodules; pushmod; cd ~/workspace; push; cd web pushgh; pacman -Qqe > $HOME/dotfiles/pkglist.txt"
 # alias apt="sudo apt-get install"
-# functions
+# functions 
+# 
 myfunction() {
     #do things with parameters like $1 such as
     git add .
