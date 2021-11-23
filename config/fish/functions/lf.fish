0feaffadd1ddabc8e5d622a5c68cfd38d2cf6cac
yashamon <yasha.savelyev@gmail.com>
2021-11-23 08:12:13 -0600

    -a

--- config/fish/functions/lf.fish
+++ config/fish/functions/lf.fish
@@ -2,7 +2,8 @@ function lfcd
 	set LF_SHELLCD_TEMPDIR '(mktemp -d -t 
   lf-shellcd-XXXXXX)'
 	export LF_SHELLCD_TEMPDIR
-	lf -last-dir-path "$LF_SHELLCD_TEMPDIR/lastdir" 
+	lf -last-dir-path "$LF_SHELLCD_TEMPDIR/lastdir" \
+		-command "source '${XDG_CONFIG_HOME:-$HOME/.config}/lf-shellcd/lfrc-shellcd'" "$@" 
 	if [ -e "$LF_SHELLCD_TEMPDIR/changecwd" ] && \
 		set dir "(cat 
     "$LF_SHELLCD_TEMPDIR/lastdir")" 2>/dev/null; then
