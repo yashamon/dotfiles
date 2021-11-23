ca07e212e7c018457466690d682a38456152f930
yashamon <yasha.savelyev@gmail.com>
2021-11-23 08:49:18 -0600

    -a

--- config/fish/functions/lfcdn.fish
+++ config/fish/functions/lfcdn.fish
@@ -4,8 +4,8 @@ function lfcdn
 	lf -last-dir-path "$LF_SHELLCD_TEMPDIR/lastdir" \
 		-command "source '$HOME/.config/lf-shellcd/lfrc-shellcd'" "$argv"
 	if [ -e "$LF_SHELLCD_TEMPDIR/changecwd" ] && \
-		set dir "(cat 
-    "$LF_SHELLCD_TEMPDIR/lastdir")" 2>/dev/null;
+		set dir (cat 
+    "$LF_SHELLCD_TEMPDIR/lastdir") 2>/dev/null;
 		cd "$dir"
  end
 	rm -rf "$LF_SHELLCD_TEMPDIR"
