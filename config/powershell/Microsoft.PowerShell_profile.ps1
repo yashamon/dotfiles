8c0aaac06ae6b8cee134790c813b0eb9b45a14d5
yashamon <yasha.savelyev@gmail.com>
2022-05-27 10:07:11 -0500

    -a

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -13,11 +13,10 @@ Get-ChildItem . -Recurse | ? { $_.PSIsContainer } | Invoke-Fzf | Set-Location
 oh-my-posh init pwsh --config /home/yasha/dotfiles/powershell/probua.minimal.omp.json | Invoke-Expression
 
 # For zoxide v0.8.0+
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook --cmd j powershell | Out-String)
-})
-
+# Invoke-Expression (& {
+#     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+#     (zoxide init --hook $hook --cmd j powershell | Out-String)
+# })
 
 # Bindings and aliases
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
