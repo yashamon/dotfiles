c35da0d63431053c047a23e94faf89bedb9d349d
yashamon <yasha.savelyev@gmail.com>
2022-05-27 10:51:40 -0500

    -a

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 $env:Path += ":/opt:$HOME/.config/sway/modules:$HOME/appimage:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:/usr/sbin:/sbin:/bin:$HOME/.local/bin:/root/.cabal/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:$HOME/.cabal/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:$HOME/.local/bin:$HOME/.local/bin/scripts:$HOME/.cargo/bin:/snap/bin:/data/data/com.termux/files/usr/bin/applets:/data/data/com.termux/files/usr/bin:bin:/usr/local/sbin:/usr/bin:$HOME/.local/share/nvim/lspinstall:$HOME/skia-binaries:$HOME/ninja:/home/yasha/.nix-profile"
 
-# Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+oh-my-posh init pwsh --config /home/yasha/dotfiles/powershell/probua.minimal.omp.json | Invoke-Expression
+
+
+# Usability
 Set-PSReadlineOption -EditMode vi
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 # example command - use $Location with a different command:
@@ -11,8 +14,8 @@ Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PsFzfOption -TabExpansion
 Set-Location (Get-ChildItem . -Recurse | ? { $_.PSIsContainer } | Invoke-Fzf) # This works as of version 2.2.8
 Get-ChildItem . -Recurse | ? { $_.PSIsContainer } | Invoke-Fzf | Set-Location
-oh-my-posh init pwsh --config /home/yasha/dotfiles/powershell/probua.minimal.omp.json | Invoke-Expression
-
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
 # For zoxide v0.8.0+
 Invoke-Expression (& {
      $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
@@ -40,7 +43,7 @@ Set-PSReadLineKeyHandler -Chord Alt+j -ScriptBlock { Invoke-PreJump }
 Set-PSReadLineKeyHandler -Key 'y' -Function Copy -ViMode Command
 Set-PSReadLineKeyHandler -Key 'p' -Function Paste -ViMode Command
 Set-PSReadLineKeyHandler -Key 'd,d' -Function DeleteLine -ViMode Command
-Set-PSReadLineKeyHandler -Key 'd,d' -Function DeleteToEnd -ViMode Command
+Set-PSReadLineKeyHandler -Key 'D' -Function DeleteToEnd -ViMode Command
 
 # Environmental variables
 $Env:EDITOR = "nvim"
