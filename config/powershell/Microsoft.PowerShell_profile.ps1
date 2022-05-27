eb6f45816dbc163a63284227633fa4c0639d2706
yashamon <yasha.savelyev@gmail.com>
2022-05-26 23:11:08 -0500

    -a

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -26,8 +26,7 @@ Function Jumphome {fdfind . $HOME -t d -H | fzf | cd}
 New-Alias z Jumphome
 Set-Alias neo $HOME/.local/bin/goneovim/goneovim
 Set-PSReadLineKeyHandler -Chord Alt+j -ScriptBlock {
-$ho="fdfind . $HOME -t f -H | fzf" | Invoke-Expression
-echo $ho
+"fdfind . $HOME -t f -H | fzf" | Invoke-Expression
 }
 
 
