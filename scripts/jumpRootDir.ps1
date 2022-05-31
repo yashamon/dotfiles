0648e917487df20a4c9c06a087cba7c2c362db8a
yashamon <yasha.savelyev@gmail.com>
2022-05-31 18:51:23 -0500

    -a

--- scripts/jumpRootDir.ps1
+++ scripts/jumpRootDir.ps1
@@ -1,3 +1,7 @@
 Invoke-Command -ScriptBlock { $res= fd . '/' -t d -H | fzf --reverse --header='Jump to location' | sed s/\\/\\\\/g
-echo $res
-lf -remote "send cd `\`"$res`\`"" }
+if ($res -notmatch '\\$') 
+            { $res+= '\'
+              echo $res }  
+lf -remote "send cd `\`"$res\\`\`""
+}
+
