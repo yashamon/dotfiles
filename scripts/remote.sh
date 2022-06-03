ee45a72fff51c5532d8ed1a315b6647909bc4ca4
yashamon <yasha.savelyev@gmail.com>
2022-06-03 13:51:53 -0500

    -a

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -1,7 +1,6 @@
 $a=$1
 $var=$(cat ~/servername.txt)
 $e="nvim --server " + "$var" + " --remote-send " + 
-'`':' 
-+ "$a" + '<cr>`''
+"`'"+':' + "$a" + '<cr>'+"`'"
 echo $e
-Invoke-Expression $e
+Invoke-Expression "$e"
