860910556675b8385a15f7a41ae5aaf2e10317eb
yashamon <yasha.savelyev@gmail.com>
2022-06-03 12:07:06 -0500

    -a

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -39,7 +39,8 @@ Set-PSReadLineKeyHandler -Chord Alt+a -Function AcceptSuggestion
 # }
 Set-Alias j Invoke-Zlocation
 Set-Alias lualatexscript /home/yasha/dotfiles/scripts/lualatexscript.ps1
-function neo() { Invoke-Expression "(which goneovim) "+"@args" }
+function neo($1) { Invoke-Expression "(which goneovim)
+"+"$1" }
 # /home/yasha/.local/bin/goneovim/goneovim
 # Set-PSReadLineKeyHandler -Chord Alt+j -ScriptBlock { Invoke-PreJump }
 Set-PSReadLineKeyHandler -Key 'y' -Function Copy -ViMode Command
