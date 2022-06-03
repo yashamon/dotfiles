d2d2a325501a80995a9a6d84666572feb47458d6
yashamon <yasha.savelyev@gmail.com>
2022-06-02 20:00:47 -0500

    -a

--- scripts/remote.ps1
+++ scripts/remote.ps1
@@ -1,4 +1,3 @@
 #!/opt/microsoft/powershell/7/pwsh
 $a=$args[0]
-echo ":$a<cr>"
-nvim --server (cat ~/servername.txt) --remote-send ":$a<cr>"
+nvim --server (cat ~/servername.txt) --remote-send ':'$a'<cr>'
