1588f10c8645dfa6fe59c3bd745d8a7185457906
yashamon <yasha.savelyev@gmail.com>
2022-06-03 13:36:06 -0500

    -a

--- scripts/remote.ps1
+++ scripts/remote.ps1
@@ -1,8 +1,11 @@
 #!/bin/pwsh
-$a=$args
+$a=$args[0]
+$b=$args[1]
 echo $a
+echo $b
 $var=(cat ~/servername.txt)
 echo $var
-$e="nvim --server " + "$var" + " --remote-send " + ':' + "$a" + '<cr>'
+$e="nvim --server " + "$var" + " --remote-send " + ':' +
+"buffer $b " + '<cr>:<esc>:' + "$a"
 echo $e
 Invoke-Expression $e
