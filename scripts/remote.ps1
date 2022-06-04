72bc12c723a65f754d1395f80ce9fdffa7f46424
yashamon <yasha.savelyev@gmail.com>
2022-06-04 14:36:40 -0500

    -a

--- scripts/remote.ps1
+++ scripts/remote.ps1
@@ -2,13 +2,11 @@
 $line=$args[0]
 $buf=$args[1]
 echo $buf > 'C:\Users\yasha\sumatra.txt'
-$file=Get-Item $buf
-$file.Basename 
 echo $line
 echo $buf
 $var=(cat ~/servername.txt)
 echo $var
 $e="nvim --server " + "$var" + " --remote-send " + "`'" + ':' +
-'buffer ' + " $file.Basename" + '<cr><esc>:' + "$line"+'<cr>'+"`'"
+'buffer ' + " $buf" + '<cr><esc>:' + "$line"+'<cr>'+"`'"
 echo $e
 Invoke-Expression $e
