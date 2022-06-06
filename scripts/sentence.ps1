e453f55380e3f8604425c409274c25447b566c28
yashamon <yasha.savelyev@gmail.com>
2022-06-06 12:53:29 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -1,11 +1,6 @@
 $f=args[0]
 
-awk '{print NR ":  "  $0 }' $f | awk -v RS= 
-'{gsub(/\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS= 
-'{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' 
-' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr 
-'\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'  > sentence_${f}
-
+awk '{print NR ":  "  $0 }' $f | awk -v RS='{gsub(/\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'  > sentence_${f}
 
 
 
--- scripts/sentence.sh
+++ scripts/sentence.sh
@@ -1,7 +1,10 @@
-#! /bin/sh 
+$f=args[0]
 
-
-awk '{print NR ":  "  $0 }' $1 | awk -v RS= '{gsub(/\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS= '{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'  > sentence_${1}
+awk '{print NR ":  "  $0 }' $f | awk -v RS= 
+'{gsub(/\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS= 
+'{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' 
+' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr 
+'\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'  > sentence_${f}
 
 
 
