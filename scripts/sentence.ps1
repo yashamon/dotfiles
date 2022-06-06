bdece7dc4155d5c5348eabc0dea06970958c401a
yashamon <yasha.savelyev@gmail.com>
2022-06-06 16:14:41 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -1,5 +1,5 @@
 $f=$args[0]
 $newfile='sentence_' + "$f"
-ForEach-Object { Add-Content -Path $f -Value "$_.:" } > $newfile
+Get-Content -Path $f | Add-Content -Path $f -Value "$_.:" > $newfile
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
 
