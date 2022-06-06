18a2e8d9add3e48f3ed499aabb31fa284337bf99
yashamon <yasha.savelyev@gmail.com>
2022-06-06 12:56:38 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -1,4 +1,4 @@
-$f=args[0]
+$f=$args[0]
 
 awk '{print NR ":  "  $0 }' $f | awk -v RS='{gsub(/\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'  > sentence_${f}
 
