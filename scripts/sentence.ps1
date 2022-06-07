052fc642ec7f6af823a7df99686314137734ad06
yashamon <yasha.savelyev@gmail.com>
2022-06-07 14:21:34 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -4,7 +4,7 @@ $a=Get-Content stableconsistency.tex | foreach { ([string]($_.ReadCount-1) + ': 
 $b=Get-Content -Raw $a
 $c=$a -replace "`n", "" | tr '\$' '' | tr '\\' ''| tr '\{' ''| tr '\}' ''|  tr '\^' '' | tr '\_' ''| tr '\%' ''
 $d=$c -replace ".\s+", "'&'`n" -replace ".@", "'&'`n"
-cat $d > blah
+echo $d > blah
 
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
 
