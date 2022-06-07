42362a244b54b9657b79a003cde2066594b39079
yashamon <yasha.savelyev@gmail.com>
2022-06-07 14:37:37 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -1,10 +1,12 @@
 $f=$args[0]
 $newfile='sentence_' + "$f"
-$a=Get-Content stableconsistency.tex | foreach { ([string]($_.ReadCount-1) + ': ' + $_  + "@") -replace ".\s+@", ".@@@" -replace ".\s+", ('. @'+[string]($_.ReadCount-1)+':+++') } 
+$a=Get-Content stableconsistency.tex | foreach {
+   ([string]($_.ReadCount-1) + ': ' + $_  + "@") -replace
+   ".\s+@", ".@@@" -replace (". "+"\s+"), ('. @'+[string]($_.ReadCount-1)+':+++') } 
 $b=[String]$a
-$c=$b -replace "`n", "" 
+# $c=$b -replace "`n", "" 
 # $d=$c -replace ('.'+"\s+"), ('&'+"`n") -replace ".@", "'&'`n"
-echo $c > blah
+echo $b > blah
 
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
 
