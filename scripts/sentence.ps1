4a66e678045ae21dd5a9f8f56d2f21b40eb6c5b0
yashamon <yasha.savelyev@gmail.com>
2022-06-06 22:38:32 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -1,7 +1,7 @@
 $f=$args[0]
 $newfile='sentence_' + "$f"
 Get-Content $f | foreach { $L=[string]($_.ReadCount-1) } | foreach { $L + ': ' + $_ } |
-foreach sed -e "|\.\s\{0,\}\n|.@???\n|g"
+sed -e "|\.\s\{0,\}\n|.@???\n|g" > blah
 
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
 
