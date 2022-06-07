c1ace575f0eece0df2272a9657cb944981ede843
yashamon <yasha.savelyev@gmail.com>
2022-06-07 15:15:31 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -6,7 +6,7 @@ $a=Get-Content stableconsistency.tex | foreach {
 $b=[String]$a
 $c=$b -replace "`n", "" 
 $d=$c -replace "\.\s+", "`n" -replace "\.@", "`n" -replace '$', ' ' -replace '\', ' ' -replace '{', ' ' -replace
-'}' ' ' -replace '^', ' ' -replace '_' ' ' -replace '%', ' '
+'}', ' ' -replace '^', ' ' -replace '_' ' ' -replace '%', ' '
 echo $d > blah
 
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
