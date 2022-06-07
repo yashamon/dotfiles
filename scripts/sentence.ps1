1be131d8be572d85cf3fd72f9c8f769eb7748ba0
yashamon <yasha.savelyev@gmail.com>
2022-06-07 14:17:59 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -4,6 +4,7 @@ $a=Get-Content stableconsistency.tex | foreach { ([string]($_.ReadCount-1) + ': 
 "@") -replace ".\s+@", ".@@@" -replace ".\s+", ('. @'+[string]($_.ReadCount-1)+':+++')
 $b=Get-Content -Raw $a
 $c=$a -replace "`n", "" | tr '\$' '' | tr '\\' ''| tr '\{' ''| tr '\}' ''|  tr '\^' '' | tr '\_' ''| tr '\%' ''
-$d=$c -replace ".\s+", "'&'`n" -replace ".@", "'&'`n" > blah
+$d=$c -replace ".\s+", "'&'`n" -replace ".@", "'&'`n"
+cat $d > blah
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
 
