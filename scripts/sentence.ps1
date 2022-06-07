af76b8343720658c501bff41d72d9d22ff60f093
yashamon <yasha.savelyev@gmail.com>
2022-06-07 17:04:51 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -4,7 +4,7 @@ $a=Get-Content stableconsistency.tex | foreach { ([string]($_.ReadCount-1) + ': 
 $b=[String]$a
 $c=$b -replace "`n", "" 
 $d=$c -replace "\.\s+", "`n" -replace "\.@", "`n" -replace
-'$', ' ' -replace '`\', ' ' 
+'$', ' ' -replace '\\', ' ' 
 # -replace '{', ' ' -replace '}', ' ' -replace '^', ' ' -replace '_', ' ' -replace '%', ' '
 echo $d > $newfile
 
