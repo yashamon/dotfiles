7d001c236d3cf03608e1cc6e4a29673eb4f2dbc8
yashamon <yasha.savelyev@gmail.com>
2022-06-07 14:55:52 -0500

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -2,9 +2,8 @@ $f=$args[0]
 $newfile='sentence_' + "$f"
 $a=Get-Content stableconsistency.tex | foreach {
    ([string]($_.ReadCount-1) + ': ' + $_  + "@") -replace
-   ".\s+@", ".@@@" }
-$k=$a -replace ".\s+", ('. @'+[string]($_.ReadCount-1)+':+++') 
-$b=[String]$k
+   ".\s+@", ".@@@" -replace ".\s+", "TEST" }
+$b=[String]$a
 # $c=$b -replace "`n", "" 
 # $d=$c -replace ('.'+"\s+"), ('&'+"`n") -replace ".@", "'&'`n"
 echo $b > blah
