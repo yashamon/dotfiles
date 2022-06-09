fe8e5e10088296e2ad02757bb2a63c4aae4a71ba
yashamon <yasha.savelyev@gmail.com>
2022-06-09 11:21:10 -0500

    -a

--- scripts/lfcdFindFile.ps1
+++ scripts/lfcdFindFile.ps1
@@ -1,4 +1,11 @@
+$res= fd . $HOME -H -I 1 | fzf --reverse --header='Jump to location' | sed 's/\\/\\\\/g' 
+if ( Test-Path -Path "$res" -PathType Container )
+     {  if ($res -notmatch '\\$') 
+            { $res+= '\'
+              echo $res }  
+        lf -remote "send cd `\`".\\$res\\`\`""
+     }
+else 
+   { lf -remote "send select `\`".\\$res`\`"" }
 
-$res= fd . $HOME -t f -H | fzf --reverse --header='Jump to location' | sed s/\\/\\\\/g
-lf -remote "send select `\`"$res\\`\`""
 
