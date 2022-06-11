89b78b31e126b361b6ddd81986b6c3e2cf9b81e5
yashamon <yasha.savelyev@gmail.com>
2022-06-11 13:28:53 -0500

    -a

--- scripts/lfLn.ps1
+++ scripts/lfLn.ps1
@@ -1,2 +1,2 @@
-foreach ($x in $args) { $f= ( Split-Path $x -leaf ); new-Item -ItemType SymbolicLink -Path "$x" -Target "./$f" }
+foreach ($x in $args) { $f= ( Split-Path $x -leaf ); new-Item -ItemType SymbolicLink -Path "./$f" -Target "$x" }
 lf -remote "send clear"
