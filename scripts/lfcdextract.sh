105ec466d9757d9aedb2bdaa5211fd06c117adee
yashamon <yasha.savelyev@gmail.com>
2022-06-11 19:19:05 -0500

    -a

--- scripts/lfcdextract.sh
+++ scripts/lfcdextract.sh
@@ -1,8 +1,8 @@
 switch (Split-Path $f -leafExtension)
 {
-        tar.bz |tar.bz2|tbz|tbz2  {tar xjvf $f}
-        tar.gz|tgz tar {xzvf $f}
-        tar.xz|txz tar {xJvf $f}
+        tar.bz -or tar.bz2 -or tbz -or tbz2  {tar xjvf $f}
+        tar.gz -or tgz {tar xzvf $f}
+        tar.xz -or txz {tar xJvf $f}
         .zip {unzip $f}
         .rar {unrar x $f}
         .7z {7z x $f}
