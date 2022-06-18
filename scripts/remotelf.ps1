5663c75a48198d63fef6c7ac6c0ccdaf4d866e61
yashamon <yasha.savelyev@gmail.com>
2022-06-17 20:24:28 -0500

    -a

--- scripts/remotelf.ps1
+++ scripts/remotelf.ps1
@@ -5,5 +5,5 @@ echo $var
 $e1="nvim --server " + "$var" + " --remote-send " + "`'" + "q" +  "`'" 
 $e2="nvim --server " + "$var" + " --remote-silent " + "$file"
 Invoke-Expression $e1
-Start-Sleep -Milliseconds 100
+Start-Sleep -Milliseconds 200
 Invoke-Expression $e2
