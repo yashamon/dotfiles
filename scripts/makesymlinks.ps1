22c2c8ba1f61bd65045b7f217e9af14d9cf7a001
yashamon <yasha.savelyev@gmail.com>
2022-08-15 19:05:55 -0500

    -a

--- scripts/makesymlinks.ps1
+++ scripts/makesymlinks.ps1
@@ -1,29 +1,24 @@
-#!tex/bin/bash
-
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc asoundrc xmonad/xmonad.hs xmonad/xmobar.hs vnc ctags tmux-conf  oh-my-zsh
-vifm/vifmrc gitconfig zshrc Xresources xprofile xinitrc latexmkrc"    # list of files/folders to symlink in homedir
+$dir=~/dotfiles                    # dotfiles directory
+$olddir=~/dotfiles_old             # old dotfiles backup directory
+$files = "ctags gitconfig latexmkrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+# echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+# mkdir -p $olddir
+# echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
-mkdir ~/.xmonad
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
