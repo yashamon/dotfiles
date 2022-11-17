1c97f5b32a4a1165c0730370bdaa264c6b633d12
yashamon <yasha.savelyev@gmail.com>
2022-11-16 21:10:21 -0600

    -a

--- scripts/sentence.ps1
+++ scripts/sentence.ps1
@@ -5,7 +5,7 @@ $b=[String]$a
 $c=$b -replace "`n", "" 
 $d=$c -replace "\.\s+", "\.@`n" -replace "\.@", "\.@`n" -replace
 '\$', ' ' -replace '\\', ' ' -replace '{', ' ' -replace '}'
-# ' ' -replace '\^', ' ' -replace '_', ' ' -replace '%', ' ' -replace '@', ' '
+' ' -replace '\^', ' ' -replace '_', ' ' -replace '%', ' ' -replace '@', ' '
 echo $d > $newfile
 
 # | sed /\.[[:space:]]*\n/,".@???\n",$0);print}'| awk -v RS='{gsub(/\.\n/,".@%%\n",$0);print}'| awk '{gsub(/\.\s/,"&"NR":++++ ",$0);print}' | tr '\$' ' ' | tr '\\' ' '| tr '\{' ' '| tr '\}' ' '|  tr '\^' ' ' | tr '\_' ' '| tr '\%' ' ' | tr '\n' ' ' | sed 's|\.\s*|&\n''|g'  | sed 's|\.@|&\n''|g'") > $newfile
