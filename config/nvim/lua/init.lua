76416a7f8f46efa0c51a6f4c9dbef993406a620d
yashamon <yasha.savelyev@gmail.com>
2022-11-29 11:07:02 -0600

    -a

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -6,67 +6,128 @@
 -- Keymaps
 vim.keymap.set('t', '<C-r>+', [[getreg('+')]], {expr = true})
 -- LSP
-require("nvim-lsp-installer").setup {}
-    local lspconfig = require("lspconfig")
-
-    local function on_attach(client, bufnr)
-        -- set up buffer keymaps, etc.
-    end
-
-    lspconfig.sumneko_lua.setup { on_attach = on_attach }
-    lspconfig.tsserver.setup { on_attach = on_attach }
-    lspconfig.vimls.setup { on_attach = on_attach }
-    lspconfig.ltex.setup { on_attach = on_attach }
-    lspconfig.texlab.setup { on_attach = on_attach }
-
-local custom_attach = function(client)
-	print("LSP started.");
-	require'completion'.on_attach(client)
-	require'diagnostic'.on_attach(client)
-
-	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
-	map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
-	map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-	map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
-	map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-	map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
-	map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-	map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
-	map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
-end
-
-lsp.tsserver.setup{on_attach=custom_attach}
-lsp.clangd.setup{on_attach=custom_attach}
-lsp.sumneko_lua.setup{
-	on_attach=custom_attach,
-	settings = {
-		Lua = {
-			runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
-			completion = { keywordSnippet = "Disable", },
-			diagnostics = { enable = true, globals = {
-				"vim", "describe", "it", "before_each", "after_each" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				}
-			}
-		}
-	}
+require'nvim-treesitter.configs'.setup {
+-- ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<m-CR>',
+      --scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
 }
 
--- Uncomment to execute the extension test mentioned above.
--- local function custom_codeAction_callback(_, _, action)
--- 	print(vim.inspect(action))
--- end
+require('leap').setup {
+  max_aot_targets = nil,
+  highlight_unlabeled = false,
+  max_highlighted_traversal_targets = 10,
+  case_sensitive = false,
+  -- Sets of characters that should match each other.
+  -- Obvious candidates are braces and quotes ('([{', ')]}', '`"\'').
+  equivalence_classes =
+  {
+      {' ', "\r", "\n" },
+      {'\'','"', '/', ':', '=', '#', '&', '%','^', '_', '<', '>', '?', '|', '!', '*', '+', '-', '`', '/', '\\', ',', '.',';', ']', '[', '}', '{', ')', '(', '$'}
+    },
+  -- Leaving the] $appropriate list emapty effectively disables "smart" mode,
+  -- and forces auto-jump to be on or off.%
+  
+}
+  require("todo-comments").setup {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
 
--- vim.lsp.callbacks['textDocument/codeAction'] = custom_codeAction_callback
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      vertical = { width = 0.9 }
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+  -- other configuration values here
+})
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.1, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = true, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  --on_colors = function(colors) 
+  --colors.normal = colors.black 
+  --end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) 
+  end,
+  
+})
+lua << EOF
+  require("which-key").setup {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+require("which-key").setup({
+ triggers = {"<leader>", "g", "z"},
+    triggers_blacklist = {
+        i = { "j", "k", "v" },
+        n = { "j", "k", "v" },
+        v = { "j", "k" }
+      },
+})
+require("gruvbox").setup({
+    overrides = {
+        SignColumn = {bg = "#ffa546"},
+        Normal = {bg = "#ffa546"},
+        DiagnosticSignError = { bg = "#ffa546" },
+        DiagnosticSignWarn = { bg = "#ffa546" }, 
+        DiagnosticSignInfo = { bg = "#ffa546" }, 
+        DiagnosticSignHint = { bg = "#ffa546" },
+        --texComment = { fg = "#00ff00" },
+        -- texString = { fg = "#0000ff" },
+        -- Special = {fg = "#0000ff" },
+        -- texCmdName = { fg = "#0000ff" },
+        -- SpecialChar = { fg = "#0000ff" },
+        -- Statement = { fg = "#0000ff" }
+
+    }
+})
+require('snippets') 
+-- load snippets from path/of/your/nvim/config/my-cool-snippets
+--vim.o.runtimepath = vim.o.runtimepath .. 'C:/Users/yasha/.config/nvim/lua/snippets,'
+-- require("luasnip/loaders/from_vscode").lazy_load() -- load snippets of friendly/snippets
+-- require("luasnip/loaders/from_vscode").load({ paths = "C:/Users/yasha/dotfiles/snippets"} ) -- load your own snippets
