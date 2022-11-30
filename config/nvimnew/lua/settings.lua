90d704bbfcfe4a9964bbe7951b9cbcd48ce5e497
yashamon <yasha.savelyev@gmail.com>
2022-11-30 09:31:57 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -5,14 +5,7 @@
 -- Keymaps
 vim.keymap.set('t', '<C-r>+', [[getreg('+')]], {expr = true})
 -- LSP
-vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-vim.api.nvim_set_hl(0, 'LeapMatch', {
-  fg = 'black',  -- for light themes, set to 'black' or similar
-  bold = true,
-  nocombine = true,
 require'nvim-treesitter.configs'.setup {
-})
-require('leap').opts.highlight_unlabeled_phase_one_targets = true  
 -- ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   incremental_selection = {
     enable = true,
