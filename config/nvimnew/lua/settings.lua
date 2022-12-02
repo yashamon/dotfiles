dd75ea25468eecd8ee9db60e172a05b9d3525a8c
yashamon <yasha.savelyev@gmail.com>
2022-12-01 18:51:31 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -190,8 +190,8 @@ undercurl = true,
         DiagnosticSignInfo = { bg = "#ffa546" }, 
         DiagnosticSignHint = { bg = "#ffa546" },
         texType = { fg = "#808080" },
-        texMath = { fg = 'red' },
-        Special = { fg = 'red' }
+        texMath = { fg = "#FF0000." },
+        Special = { fg = "#FF0000." }
  
 
         --texComment = { fg = "#00ff00" },
