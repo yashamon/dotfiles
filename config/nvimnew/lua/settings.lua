874903289c47a156e170655ae56d4cccc960c65d
yashamon <yasha.savelyev@gmail.com>
2022-12-03 15:57:52 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -195,7 +195,7 @@ undercurl = true,
         texDelimiter = { fg = "#808080" },
         texStatement = { fg = "#808080" },
         -- Operator = { fg = "#808080" },
-        -- Type = { fg = "#008800" },
+        Type = { fg = "#008800" },
         Comment = { fg = "#af6000" },
         String = { fg = "#580058" },
         Operator = { fg = "#580058" }
