01aefc69ba0b9825c0c17424625b76dc5e7ca283
yashamon <yasha.savelyev@gmail.com>
2022-12-02 22:08:38 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -197,7 +197,7 @@ undercurl = true,
         -- Operator = { fg = "#808080" },
         -- Type = { fg = "#008800" },
         Comment = { fg = "#af6000" },
-        String = { fg = "#770000" }
+        String = { fg = "#630000 " }
 
 
 
