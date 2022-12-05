28599b9c6f7a4da91aa1790b7b824fffd17217e6
yashamon <yasha.savelyev@gmail.com>
2022-12-05 11:03:44 -0600

    -a

--- config/nvimnew/lua/snippets.lua
+++ config/nvimnew/lua/snippets.lua
@@ -135,17 +135,23 @@ end
 
 local date = function() return {os.date('%Y-%m-%d')} end
 
-ls.add_snippets(nil, {
-    all = {
-        snip({
-            trig = "date",
-            namr = "Date",
-            dscr = "Date in the form of YYYY-MM-DD",
-        }, 
-        {"test"}
-        })
-
-     })
+snip({
+    trig = "meta",
+    namr = "Metadata",
+    dscr = "Yaml metadata format for markdown"
+},
+{
+    text({"---",
+    "title: "}), insert(1, "note_title"), text({"", 
+    "author: "}), insert(2, "author"), text({"", 
+    "date: "}), func(date, {}), text({"",
+    "categories: ["}), insert(3, ""), text({"]",
+    "lastmod: "}), func(date, {}), text({"",
+    "tags: ["}), insert(4), text({"]",
+    "comments: true",
+    "---", ""}),
+    insert(0)
+  }),
 
 
 --[[
