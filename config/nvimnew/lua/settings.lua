f87b200f4489599e7e30020db9bccf6b21bd8d57
yashamon <yasha.savelyev@gmail.com>
2022-12-11 22:47:01 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -279,7 +279,17 @@ end
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
-
+require('snippy').setup({
+    mappings = {
+        is = {
+            ['<Tab>'] = 'expand_or_advance',
+            ['<S-Tab>'] = 'previous',
+        },
+        nx = {
+            ['<leader>x'] = 'cut_text',
+        },
+    },
+})
 
 local snippy = require("snippy")
 local luasnip = require("luasnip")
@@ -301,7 +311,7 @@ mapping = cmp.mapping.preset.insert({
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
 -- ... Your other mappings ...
 ["<Tab>"] = cmp.mapping(function(fallback)
-			if snippy#can_expand_or_advance() then
+			if snippy.can_expand_or_advance() then
 				snippy-expand()
                         elseif cmp.visible() then
                              cmp.select_next_item()
