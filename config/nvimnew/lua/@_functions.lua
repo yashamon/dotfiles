c77365b4344e2cc43937c2eb727a62d8b2cd1f04
yashamon <yasha.savelyev@gmail.com>
2022-12-19 14:32:36 -0600

    -a

--- config/nvimnew/lua/@_functions.lua
+++ config/nvimnew/lua/@_functions.lua
@@ -16,28 +16,27 @@
  C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:16::       require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9)
  C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:17::    vim.cmd('cg @_%')
  C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:18::    vim.cmd('copen')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:19::    vim.cmd('sleep 600m')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:20::    if vim.b.bqf_enabled then
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:21::     vim.defer_fn(function()
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:22::         vim.api.nvim_feedkeys('zf', 'im', false)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:23::     end, 10)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:24:: end
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:25::       vim.cmd('sleep 600m')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:26::       vim.fn.feedkeys("zf")
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:27::       " copen
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:28::       " sleep 600m
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:29::       " " let b:paste = system('pwsh  c Get Clipboard')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:30::       " " call feedkeys(":BLinesB  <c r>+ <cr>")
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:31::       " " call feedkeys("zf")
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:32:: end
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:33:: 
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:34:: Sentence = function ()
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:35::    local filename = vim.fn.substitute(vim.fn.expand('%:p'), "  ", "/", "g")
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:36::    local execstr = "nu C:/Users/yasha/dotfiles/scripts/sentence.nu " .. filename
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:37::       print(execstr)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:38::    os.execute(execstr)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:39::    vim.cmd('cg @_%')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:40::    vim.cmd('copen')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:41::       vim.cmd('sleep 600m')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:42::       vim.fn.feedkeys("zf")
- C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:43:: end
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:19::    if vim.b.bqf_enabled then
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:20::     vim.defer_fn(function()
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:21::         vim.api.nvim_feedkeys('zf', 'im', false)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:22::     end, 600)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:23:: end
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:24::       vim.cmd('sleep 600m')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:25::       vim.fn.feedkeys("zf")
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:26::       " copen
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:27::       " sleep 600m
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:28::       " " let b:paste = system('pwsh  c Get Clipboard')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:29::       " " call feedkeys(":BLinesB  <c r>+ <cr>")
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:30::       " " call feedkeys("zf")
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:31:: end
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:32:: 
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:33:: Sentence = function ()
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:34::    local filename = vim.fn.substitute(vim.fn.expand('%:p'), "  ", "/", "g")
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:35::    local execstr = "nu C:/Users/yasha/dotfiles/scripts/sentence.nu " .. filename
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:36::       print(execstr)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:37::    os.execute(execstr)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:38::    vim.cmd('cg @_%')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:39::    vim.cmd('copen')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:40::       vim.cmd('sleep 600m')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:41::       vim.fn.feedkeys("zf")
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/functions.lua:42:: end
