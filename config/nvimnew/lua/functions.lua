292db505c24dbbeb37a321dc42101bfdc96bfc00
yashamon <yasha.savelyev@gmail.com>
2022-12-18 20:12:37 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -1,6 +1,7 @@
 Server = function ()
    local buff = vim.cmd('call bufname()')
-   local servername = vim.cmd('echo v:servername') 
+   local servername = vim.cmd('echo v:servername')
+   vim.cmd('echo servername')
    os.execute("echo " .. servername .. ' > C:/Users/yasha/servername.txt')
    os.execute("echo " .. buff .. ' > C:/Users/yasha/filename.txt')
 end
