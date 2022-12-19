5d15e678dc7613d89e62789b7bf02555acac8ab3
yashamon <yasha.savelyev@gmail.com>
2022-12-19 10:53:57 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -4,3 +4,40 @@ Server = function ()
    os.execute("echo " .. servername .. ' > C:/Users/yasha/servername.txt')
    os.execute("echo " .. buff .. ' > C:/Users/yasha/filename.txt')
 end
+
+Line = function()
+   local buff = vim.cmd('call bufname()')
+   local filename = vim.fn.expand('%:p:h')
+   local execstr = "!nu C:/Users/yasha/dotfiles/scripts/line.nu " .. filename
+   os.execute(execstr)
+   vim.cmd('cg @_%')   
+   require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9}})
+end
+--    let b:filenamedir = substitute(expand('%:p:h'), "\\", "/", "g")
+--    let b:file = expand('%:p')
+--    let b:filename = substitute(b:file, "\\", "/", "g")
+--    let b:execstr = "!nu C:/Users/yasha/dotfiles/scripts/line.nu " . b:filename 
+--    exec b:execstr
+--    cg @_%
+--    lua require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9}}) 
+--    " copen
+--    " sleep 600m
+--    " " let b:paste = system('pwsh -c Get-Clipboard')
+--    " " call feedkeys(":BLinesB \<c-r>+\<cr>")
+--    " call feedkeys("zf")
+-- endfunction
+-- function Sentence()
+--    let g:buf = bufname()
+--    " silent execute "!bash /mnt/c/Users/yasha/dotfiles/scripts/sentence.sh %"
+--    let b:filenamedir = substitute(expand('%:p:h'), "\\", "/", "g")
+--    let b:file = expand('%:p')
+--    let b:filename = substitute(b:file, "\\", "/", "g")
+--    let b:execstr = "!nu C:/Users/yasha/dotfiles/scripts/sentence.nu " . b:filename
+--    exec b:execstr
+--    cg @_%
+--    lua require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9}}) 
+--    " copen
+--    " sleep 600m
+--    " " let b:paste = system('pwsh -c Get-Clipboard')
+--    " call feedkeys("zf")
+-- endfunction
