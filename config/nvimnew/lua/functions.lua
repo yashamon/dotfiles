2592153a8747cc82ce2902dbd064c4a0fde722a8
yashamon <yasha.savelyev@gmail.com>
2022-12-19 11:47:45 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -9,36 +9,24 @@ Line = function()
    -- local buff = vim.cmd('call bufname()')
    local filename = vim.fn.substitute(vim.fn.expand('%:p'), "\\", "/", "g")
    local execstr = "nu C:/Users/yasha/dotfiles/scripts/line.nu " .. filename
-   print(execstr)
+   -- print(execstr)
    os.execute(execstr)
    vim.cmd('cg @_%')   
    require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9}})
+   -- " copen
+   -- " sleep 600m
+   -- " " let b:paste = system('pwsh -c Get-Clipboard')
+   -- " " call feedkeys(":BLinesB \<c-r>+\<cr>")
+   -- " " call feedkeys("zf")
 end
---    let b:filenamedir = substitute(expand('%:p:h'), "\\", "/", "g")
---    let b:file = expand('%:p')
---    let b:filename = substitute(b:file, "\\", "/", "g")
---    let b:execstr = "!nu C:/Users/yasha/dotfiles/scripts/line.nu " . b:filename 
---    exec b:execstr
---    cg @_%
---    lua require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9}}) 
---    " copen
---    " sleep 600m
---    " " let b:paste = system('pwsh -c Get-Clipboard')
---    " " call feedkeys(":BLinesB \<c-r>+\<cr>")
---    " call feedkeys("zf")
--- endfunction
--- function Sentence()
---    let g:buf = bufname()
---    " silent execute "!bash /mnt/c/Users/yasha/dotfiles/scripts/sentence.sh %"
---    let b:filenamedir = substitute(expand('%:p:h'), "\\", "/", "g")
---    let b:file = expand('%:p')
---    let b:filename = substitute(b:file, "\\", "/", "g")
---    let b:execstr = "!nu C:/Users/yasha/dotfiles/scripts/sentence.nu " . b:filename
---    exec b:execstr
---    cg @_%
---    lua require('telescope.builtin').quickfix({layout_strategy='vertical',layout_config={width=0.9}}) 
---    " copen
---    " sleep 600m
---    " " let b:paste = system('pwsh -c Get-Clipboard')
---    " call feedkeys("zf")
--- endfunction
+
+Sentence = function ()
+   local filename = vim.fn.substitute(vim.fn.expand('%:p'), "\\", "/", "g")
+   local execstr = "nu C:/Users/yasha/dotfiles/scripts/sentence.nu " .. filename
+   -- print(execstr)
+   os.execute(execstr)
+   vim.cmd('cg @_%')
+   vim.cmd('copen')
+   vim.cmd('sleep 600m')
+   vim.fn.feedkey("zf")
+end
