5cd31c47e57965d65ace5a3b628560ae0c6f4161
yashamon <yasha.savelyev@gmail.com>
2022-12-18 19:12:32 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -2,7 +2,7 @@ Server = function ()
    local buffmain = vim.cmd('call bufname()')
    vim.cmd('echo buffmain') 
 end
-server()
+Server()
 --    let g:buffmain=bufname()
 --    echo g:buffmain
 --    silent execute "!echo " . v:servername . ' > C:/Users/yasha/servername.txt'
