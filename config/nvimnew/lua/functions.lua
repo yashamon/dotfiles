bea8c3dd543e30ed8cec8d8b94b52938fbc0bdb9
yashamon <yasha.savelyev@gmail.com>
2022-12-18 20:23:04 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -4,9 +4,3 @@ Server = function ()
    os.execute("echo " .. servername .. ' > C:/Users/yasha/servername.txt')
    os.execute("echo " .. buff .. ' > C:/Users/yasha/filename.txt')
 end
---    let g:buffmain=bufname()
---    echo g:buffmain
---    silent execute "!echo " . v:servername . ' > C:/Users/yasha/servername.txt'
---    silent execute "!echo " . g:buffmain . ' > C:/Users/yasha/filename.txt'
--- endfunction
--- nmap <leader>ser :call Server()
