79fc953d403a6d84a23ac659598637e5d1266d87
yashamon <yasha.savelyev@gmail.com>
2022-12-19 22:33:17 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -119,26 +119,26 @@ require("tokyonight").setup({
   -- on_highlights = function(highlights, colors)
   -- end,
 })
--- require("which-key").setup({
---   plugins = {
---     marks = true, -- shows a list of your marks on ' and `
---     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
---     spelling = {
---       enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
---       suggestions = 50, -- how many suggestions should be shown in the list?
---     },
---     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
---     -- No actual key bindings are created
---     presets = {
---       operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
---       motions = true, -- adds help for motions
---       text_objects = true, -- help for text objects triggered after entering an operator
---       windows = true, -- default bindings on <c-w>
---       nav = true, -- misc bindings to work with windows
---       z = true, -- bindings for folds, spelling and others prefixed with z
---       g = true, -- bindings for prefixed with g
---     },
---   },
+require("which-key").setup({
+  plugins = {
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    spelling = {
+      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 50, -- how many suggestions should be shown in the list?
+    },
+    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+    -- No actual key bindings are created
+    presets = {
+      operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = true, -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
+      windows = true, -- default bindings on <c-w>
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
+  },
   -- add operators that will trigger motion and text object completion
   -- to enable all native operators, set the preset / operators plugin above
   operators = { gc = "Comments" },
@@ -175,7 +175,7 @@ require("tokyonight").setup({
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
   show_keys = true, -- show the currently pressed key and its label as a message in the command line
-  triggers = {"<leader>", "g", "z", ";"}, -- automatically setup triggers
+  triggers = {"<leader>", "g", ";"}, -- automatically setup triggers
   -- triggers = {"<leader>"} -- or specify a list manually
   triggers_blacklist = {
     -- list of mode / prefixes that should never be hooked by WhichKey
