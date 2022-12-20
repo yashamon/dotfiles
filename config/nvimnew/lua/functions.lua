912249cdde06e6652ac50aa5114de960a1faa5a2
yashamon <yasha.savelyev@gmail.com>
2022-12-19 23:12:15 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -9,7 +9,7 @@ Line = function()
    local filename = vim.fn.substitute(vim.fn.expand('%:p'), "\\", "/", "g")
    local filenameshort = vim.fn.substitute(vim.fn.expand('%'), "\\", "/", "g")
    local execstr = "nu C:/Users/yasha/dotfiles/scripts/line.nu " .. filename
-   -- os.execute(execstr)
+   os.execute(execstr)
    local command = "cg " .. "@_" .. filenameshort
    vim.cmd(command)
    vim.cmd('copen')
