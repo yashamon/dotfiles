f1a00e6ae21891f73a829d208d9cd6d146480a78
yashamon <yasha.savelyev@gmail.com>
2022-12-19 21:40:21 -0600

    -a

--- config/nvimnew/after/ftplugin/qf.lua
+++ config/nvimnew/after/ftplugin/qf.lua
@@ -1,5 +1,6 @@
 if vim.b.bqf_enabled then
     vim.defer_fn(function()
-        vim.api.nvim_feedkeys('zf\<BS>\<BS>', 'i', false)
+        let key = nvim_replace_termcodes("zf\<BS>\<BS>", v:true, v:false, v:true)
+        vim.api.nvim_feedkeys('key', 'i', false)
     end, 10)
 end
