29ee5cb54d3c0a3246c54c82821238f350a822c6
yashamon <yasha.savelyev@gmail.com>
2022-12-19 23:15:56 -0600

    -a

--- config/nvimnew/after/ftplugin/qf.lua
+++ config/nvimnew/after/ftplugin/qf.lua
@@ -1,6 +1,7 @@
 if vim.b.bqf_enabled then
+    vim.cmd('sleep 100m')
     vim.defer_fn(function()
-       vim.api.nvim_input("zf<BS><BS>")
+       vim.api.nvim_input("zf")
         -- vim.api.nvim_feedkeys('zf', 'i', false)
     end, 10)
 end
