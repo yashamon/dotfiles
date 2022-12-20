b95eab32ca13836f5930858fc6205ee7e789dddc
yashamon <yasha.savelyev@gmail.com>
2022-12-19 22:35:30 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -16,6 +16,7 @@ Line = function()
    local command = "cg " .. "@_" .. filenameshort
    vim.cmd(command)
    vim.cmd('copen')
+   call setqflist([])
 --    if vim.b.bqf_enabled then
 --     vim.defer_fn(function()
 --         vim.api.nvim_feedkeys('zf', 'im', false)
