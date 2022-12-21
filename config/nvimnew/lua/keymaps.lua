827ab36a5e0205ea0683cab8e8792fc3fffe1594
yashamon <yasha.savelyev@gmail.com>
2022-12-20 21:41:31 -0600

    -a

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -1,8 +1,10 @@
 vim.keymap.set('t', '<m-p>', [[getreg('+')]], {expr = true})
 vim.keymap.set({'i','n'}, '<m-t>', function ()  vim.cmd('up')
    vim.cmd('BTags')
-   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q>', true, false, true), 'i', false)
-   vim.cmd('sleep 200m')
-   vim.cmd('copen') end, { buffer=true })
+   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q><C-w>d', true, false, true), 'i', false)
+   if vim.w.bqf_enabled then
+    vim.api.nvim_feedkeys('zf', 'im', false)
+end
+   end, { buffer=true })
 
 -- nnoremap <m-t> :up<cr>:BTags<cr>:call feedkeys("\<c-a>\<c-q>")<cr>:copen<cr>
