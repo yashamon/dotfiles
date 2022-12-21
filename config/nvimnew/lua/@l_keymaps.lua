4e5d5a57c10e2446535866c585caba05784165cf
yashamon <yasha.savelyev@gmail.com>
2022-12-20 21:43:12 -0600

    -a

--- config/nvimnew/lua/@l_keymaps.lua
+++ config/nvimnew/lua/@l_keymaps.lua
@@ -1,10 +1,11 @@
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:1:: vim.keymap.set('t', '<m p>', [[getreg('+')  , {expr = true)
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:2:: vim.keymap.set({'i','n', '<m t>', function ()  vim.cmd('up')
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:3::    vim.cmd('BTags')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:4::    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C a><C q><C w>j', true, false, true), 'i', false)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:5::    if vim.w.bqf_enabled then
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:6::     vim.api.nvim_feedkeys('zf', 'im', false)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:7:: end
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:8::    end, { buffer=true )
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:9:: 
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:10::    nnoremap <m t> :up<cr>:BTags<cr>:call feedkeys(" <c a> <c q>")<cr>:copen<cr>
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:4::    vim.cmd('sleep 100m')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:5::    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C a><C q><C w>j', true, false, true), 'i', false)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:6::    if vim.w.bqf_enabled then
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:7::     vim.api.nvim_feedkeys('zf', 'im', false)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:8:: end
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:9::    end, { buffer=true )
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:10:: 
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:11::    nnoremap <m t> :up<cr>:BTags<cr>:call feedkeys(" <c a> <c q>")<cr>:copen<cr>
--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('t', '<m-p>', [[getreg('+')]], {expr = true})
 vim.keymap.set({'i','n'}, '<m-t>', function ()  vim.cmd('up')
    vim.cmd('BTags')
+   vim.cmd('sleep 100m')
    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q><C-w>j', true, false, true), 'i', false)
    if vim.w.bqf_enabled then
     vim.api.nvim_feedkeys('zf', 'im', false)
