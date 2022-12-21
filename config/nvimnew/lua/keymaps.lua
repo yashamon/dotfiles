901247208f5f7acf1597e0f9a75a5e8658a7597a
yashamon <yasha.savelyev@gmail.com>
2022-12-20 21:37:33 -0600

    -a

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -1,7 +1,6 @@
 vim.keymap.set('t', '<m-p>', [[getreg('+')]], {expr = true})
 vim.keymap.set({'i','n'}, '<m-t>', function ()  vim.cmd('up')
    vim.cmd('BTags')
-   vim.cmd('sleep 200m')
    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q>', true, false, true), 'i', false)
    vim.cmd('sleep 200m')
    vim.cmd('copen') end, { buffer=true })
