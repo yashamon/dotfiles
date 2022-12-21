508dc8b11223f2d570b8e41893f7815e512842ea
yashamon <yasha.savelyev@gmail.com>
2022-12-20 22:02:45 -0600

    -a

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -1,17 +1,18 @@
 vim.keymap.set('t', '<m-p>', [[getreg('+')]], {expr = true})
 vim.keymap.set({'i','n'}, '<m-t>', function ()  vim.cmd('up')
+   vim.cmd('up')
    vim.cmd('BTags')
-   vim.cmd('sleep 100m')
-   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q>', true, false, true), 'i', false)
-   Wait  = function ()
-   if vim.w.bqf_enabled then
-      vim.cmd('sleep 100m')
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>jzf', true, false, true), 'im', false)
-      else
-      Wait()
-   end
-end
-   -- Wait()
+--    vim.cmd('sleep 100m')
+--    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q>', true, false, true), 'i', false)
+--    Wait  = function ()
+--    if vim.w.bqf_enabled then
+--       vim.cmd('sleep 100m')
+--       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>jzf', true, false, true), 'im', false)
+--       else
+--       Wait()
+--    end
+-- end
+--    -- Wait()
    end, { buffer=true })
 
 -- nnoremap <m-t> :up<cr>:BTags<cr>:call feedkeys("\<c-a>\<c-q>")<cr>:copen<cr>
