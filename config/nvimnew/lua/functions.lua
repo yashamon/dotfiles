13fcae9d1084064898b475cbcb84d7eeb53ca413
yashamon <yasha.savelyev@gmail.com>
2022-12-20 20:09:42 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -53,4 +53,23 @@ vim.cmd("buffer " .. buff)
    local commandline = "cg " .. "@l_" .. filenameshort
    vim.cmd(commandline)
 end
+
+local fzf = require "fzf".fzf
+local action = require "fzf.actions".action
+
+coroutine.wrap(function()
+  -- items is a table of selected or hovered fzf items
+  local shell = action(function(items, fzf_lines, fzf_cols)
+    -- only one item will be hovered at any time, so get the selection
+    -- out and convert it to a number
+    local buf = tonumber(items[1])
+
+    -- you can return either a string or a table to show in the preview
+    -- window
+    return vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  end)
+
+  fzf(vim.api.nvim_list_bufs(), "--preview " .. shell)
+end)()
+
 -- asdf
