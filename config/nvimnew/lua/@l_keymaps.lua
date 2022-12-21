ef7ffce3fd8f8016b97d8f06fe7caa9c3e2cccde
yashamon <yasha.savelyev@gmail.com>
2022-12-20 21:41:38 -0600

    -a

--- config/nvimnew/lua/@l_keymaps.lua
+++ config/nvimnew/lua/@l_keymaps.lua
@@ -1,7 +1,7 @@
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:1:: vim.keymap.set('t', '<m p>', [[getreg('+')  , {expr = true)
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:2:: vim.keymap.set({'i','n', '<m t>', function ()  vim.cmd('up')
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:3::    vim.cmd('BTags')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:4::    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C a><C q><C w>d', true, false, true), 'i', false)
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:4::    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C a><C q><C w>j', true, false, true), 'i', false)
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:5::    if vim.w.bqf_enabled then
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:6::     vim.api.nvim_feedkeys('zf', 'im', false)
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:7:: end
