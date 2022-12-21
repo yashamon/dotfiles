980aa8c7b84803f53ae1524e9d41800849bb1a7c
yashamon <yasha.savelyev@gmail.com>
2022-12-20 21:34:41 -0600

    -a

--- config/nvimnew/lua/@l_keymaps.lua
+++ config/nvimnew/lua/@l_keymaps.lua
@@ -1,9 +1,9 @@
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:1:: vim.keymap.set('t', '<m p>', [[getreg('+')  , {expr = true)
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:2:: vim.keymap.set({'i','n', '<m t>', function ()  vim.cmd('up')
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:3::    vim.cmd('BTags')
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:4::    vim.cmd('sleep 100m')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:4::    vim.cmd('sleep 200m')
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:5::    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C a><C q>', true, false, true), 'i', false)
- C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:6::    vim.cmd('sleep 30m')
+ C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:6::    vim.cmd('sleep 200m')
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:7::    vim.cmd('copen') end, { buffer=true )
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:8:: 
  C:/Users/yasha/dotfiles/config/nvimnew/lua/keymaps.lua:9::    nnoremap <m t> :up<cr>:BTags<cr>:call feedkeys(" <c a> <c q>")<cr>:copen<cr>
--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -1,9 +1,9 @@
 vim.keymap.set('t', '<m-p>', [[getreg('+')]], {expr = true})
 vim.keymap.set({'i','n'}, '<m-t>', function ()  vim.cmd('up')
    vim.cmd('BTags')
-   vim.cmd('sleep 100m')
+   vim.cmd('sleep 200m')
    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-a><C-q>', true, false, true), 'i', false)
-   vim.cmd('sleep 30m')
+   vim.cmd('sleep 200m')
    vim.cmd('copen') end, { buffer=true })
 
 -- nnoremap <m-t> :up<cr>:BTags<cr>:call feedkeys("\<c-a>\<c-q>")<cr>:copen<cr>
