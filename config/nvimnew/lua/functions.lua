303881ef7dac8ac824755d559970e22463cdfd65
yashamon <yasha.savelyev@gmail.com>
2022-12-21 23:38:44 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -1,3 +1,6 @@
+Feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
 Server = function ()
    local buff = vim.fn.bufname()
    local servername = vim.api.nvim_eval("v:servername")
