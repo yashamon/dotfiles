07e0c3f63ed5161db71c1a0f4c3b8a7b1772d4d0
yashamon <yasha.savelyev@gmail.com>
2022-12-21 21:28:28 -0600

    -a

--- config/nvimnew/after/ftplugin/@l_qf.lua
+++ config/nvimnew/after/ftplugin/@l_qf.lua
@@ -1,6 +1,6 @@
- C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:1:: if vim.w.bqf_enabled then
- C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:2::     vim.api.nvim_feedkeys('zf', 'im', false)
- C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:3:: end
+ C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:1::    if vim.w.bqf_enabled then
+ C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:2::        vim.api.nvim_feedkeys('zf', 'im', false)
+ C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:3::    end
  C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:4:: 
  C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:5::    if vim.b.bqf_enabled then
  C:/Users/yasha/dotfiles/config/nvimnew/after/ftplugin/qf.lua:6::        vim.defer_fn(function()
--- config/nvimnew/after/ftplugin/qf.lua
+++ config/nvimnew/after/ftplugin/qf.lua
@@ -1,6 +1,6 @@
-if vim.w.bqf_enabled then
-    vim.api.nvim_feedkeys('zf', 'im', false)
-end
+-- if vim.w.bqf_enabled then
+    -- vim.api.nvim_feedkeys('zf', 'im', false)
+-- end
 
 -- if vim.b.bqf_enabled then
     -- vim.defer_fn(function()
