a7edb72fc52d64205adeed51ee2fab75a1fad0d1
yashamon <yasha.savelyev@gmail.com>
2022-12-22 21:05:39 -0600

    -a

---
+++ config/nvimnew/lua/set.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+opt.nobackup = true
+opt.ignorecase = true
+opt.smartcase = true
+--
+-- " set ignorecase		" ignore case when searching
+-- set smartcase		" ignore case if search pattern is all lowercase,case-sensitive otherwise
+-- set spell   
+-- opt.
