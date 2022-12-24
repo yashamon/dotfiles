9948d931c9623b6113294133c1ddcbe2dd18ea90
yashamon <yasha.savelyev@gmail.com>
2022-12-24 10:28:44 -0600

    -a

--- config/nvimnew/after/ftplugin/qf.lua
+++ config/nvimnew/after/ftplugin/qf.lua
@@ -7,3 +7,15 @@
         -- vim.api.nvim_feedkeys('zf', 'i', false)
     -- end, 50)
 -- end
+if vim.w.bqf_enabled then
+    local savedOpts = vim.env.FZF_DEFAULT_OPTS
+    vim.env.FZF_DEFAULT_OPTS = vim.env.FZF_DEFAULT_OPTS .. ' -q pattern'
+    vim.api.nvim_feedkeys('zf', 'im', false)
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'fzf',
+        once = true,
+        callback = function()
+            vim.env.FZF_DEFAULT_OPTS = savedOpts
+        end
+    })
+end
