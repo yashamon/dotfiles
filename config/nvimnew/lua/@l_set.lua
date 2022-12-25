4bbd3fcd752f4d3829c5cee3eb66c687e6de40bb
yashamon <yasha.savelyev@gmail.com>
2022-12-24 22:29:07 -0600

    -a

--- config/nvimnew/lua/@l_set.lua
+++ config/nvimnew/lua/@l_set.lua
@@ -5,4 +5,3 @@
  C:/Users/yasha/dotfiles/config/nvimnew/lua/set.lua:5::    opt.guicursor = n v c:block,i ci ve:ver25,r cr:hor20,o:hor50,a:blinkwait700 blinkoff400 blinkon250 Cursor/lCursor,sm:block blinkwait175 blinkoff150 blinkon175 
  C:/Users/yasha/dotfiles/config/nvimnew/lua/set.lua:6::   
  C:/Users/yasha/dotfiles/config/nvimnew/lua/set.lua:7:: opt.spell = true
- C:/Users/yasha/dotfiles/config/nvimnew/lua/set.lua:8::    opt.
