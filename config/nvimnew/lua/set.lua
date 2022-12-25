ba8c0301187505bd7afae2d541d8a6196a40ba36
yashamon <yasha.savelyev@gmail.com>
2022-12-24 22:36:06 -0600

    -a

--- config/nvimnew/lua/set.lua
+++ config/nvimnew/lua/set.lua
@@ -13,7 +13,9 @@ opt.title = true
 opt.cmdheight = 0
 opt.signcolumn = true
 opt.scrolloff=10
-
+opt.pumblend=20
+ -- set winblend=15 
+opt.switchbuf='newtab'
 -- opt.ignorecase = true
 opt.smartcase = true
 -- opt.guicursor = n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175 
