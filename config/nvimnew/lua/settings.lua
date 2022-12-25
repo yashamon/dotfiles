a68615d6e1156e04f21c0864ef9144692d8cbe4b
yashamon <yasha.savelyev@gmail.com>
2022-12-24 22:04:58 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -306,15 +306,15 @@ mapping = cmp.mapping.preset.insert({
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
 -- ... Your other mappings ...
-["<C-Tab>"] = cmp.mapping(function()
+["<Tab>"] = cmp.mapping(function(fallback)
 			if luasnip.expandable() then
 				luasnip.expand()
                         elseif cmp.visible() then
                              cmp.select_next_item()
 			-- elseif has_words_before() then
 			-- 	cmp.complete()
-			--                              else
-			-- 	fallback()
+                             else
+				fallback()
 			end
 		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
