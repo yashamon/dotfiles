d1cd2653467fba108993a3b87267dd26906768a2
yashamon <yasha.savelyev@gmail.com>
2022-12-24 22:28:37 -0600

    -a

--- config/nvimnew/lua/set.lua
+++ config/nvimnew/lua/set.lua
@@ -1,6 +1,6 @@
 local opt = vim.opt
 vim.opt.backup = false
-opt.ignorecase = true
+-- opt.ignorecase = true
 opt.smartcase = true
 -- opt.guicursor = n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175 
 --
