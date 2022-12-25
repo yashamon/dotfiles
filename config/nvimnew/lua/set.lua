20cc4dbff7e8b4ca7e34441f97478baddecb7060
yashamon <yasha.savelyev@gmail.com>
2022-12-24 22:32:11 -0600

    -a

--- config/nvimnew/lua/set.lua
+++ config/nvimnew/lua/set.lua
@@ -4,6 +4,7 @@ vim.opt.tabstop = indent
 vim.opt.shiftwidth = indent
 vim.opt.softtabstop = indent
 vim.opt.backup = false
+-- vim.clipboard = unnamedplus
 -- opt.ignorecase = true
 opt.smartcase = true
 -- opt.guicursor = n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175 
