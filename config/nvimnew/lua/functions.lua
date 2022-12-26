e686ae3ad4bfaa5ed7dd7d43d74031db64b58b69
yashamon <yasha.savelyev@gmail.com>
2022-12-25 18:35:35 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -74,7 +74,15 @@ local timer = 0
     require('cmp').complete({ reason = require('cmp').ContextReason.Auto })
   end
 
-
+-- autocommands
+vim.api.nvim_create_autocmd(
+    { "BufReadPost","FileReadPost", "FileType tex, vim, lua"},
+    {
+      callback = function() GitAsync()
+			end,
+      pattern = "*",
+    }
+  )
   vim.api.nvim_create_autocmd(
     { "TextChangedI", },
     {
