1287fe51edafcbf72391798923a0d10a02150a18
yashamon <yasha.savelyev@gmail.com>
2022-12-26 20:21:57 -0600

    -a

--- config/nvimnew/lua/set.lua
+++ config/nvimnew/lua/set.lua
@@ -60,3 +60,9 @@ vim.opt.runtimepath:append(',/tmp/pluggedtest/vim-plug')
 -- opt.guicursor = n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175 
 --
 opt.spell = true
+-- highlights
+
+lua vim.api.nvim_set_hl(0, 'IndentBlanklineSpaceChar', { fg = '#00FF00' })
+ 
+ 
+ highlight IndentBlanklineSpaceChar guifg=#00FF00 gui=nocombine
