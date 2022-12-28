0ca38c48ffe59d9a1c523bea41e7a55903d87796
yashamon <yasha.savelyev@gmail.com>
2022-12-28 09:08:10 -0600

    -a

--- config/nvimnew/lua/autocommand.lua
+++ config/nvimnew/lua/autocommand.lua
@@ -27,7 +27,7 @@ au('FileType', {pattern = 'vim', command = 'set list'})
 au('FileType', {pattern = 'tex', command = 'vim.opt.wrapmargin=0'})
 au('BufWritePost', { callback = function() GitAsync() end })
 au('BufReadPost, FileReadPost', {pattern = 'tex,lua,vim', callback = CG })
-au('FileType', { pattern = 'tex', command = 'vmap q xi<CR><CR><CR><CR><ESC>kki/begin{comment}<cr><cr>/end{comment}<esc>kp'})
+au('FileType', { pattern = 'tex', command = 'vmap q xi<CR><CR><CR><CR><ESC>kki/begin{comment}<cr><cr>/end{comment}<esc>kp'} )
 local yankGrp = ag("YankHighlight", { clear = true })
 au("TextYankPost", {
   command = "silent! lua vim.highlight.on_yank()",
