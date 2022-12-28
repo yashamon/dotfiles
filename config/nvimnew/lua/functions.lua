35a0bd09a873e0c6bc1c99a0d844c3a3c7c64d17
yashamon <yasha.savelyev@gmail.com>
2022-12-27 20:18:34 -0600

    -a

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -54,10 +54,10 @@ CG = function ()
    Idline = vim.fn.jobstart({"nu", "C:/Users/yasha/dotfiles/scripts/Line.nu", filename})
    IdSentence = vim.fn.jobstart({"nu", "C:/Users/yasha/dotfiles/scripts/sentence.nu", filename})
    vim.fn.jobwait({IdSentence})
-   local commandsentence = "lg " .. "@s_" .. filenameshort
+   local commandsentence = "lg " .. "/tmp/@s_" .. filenameshort
    vim.cmd(commandsentence)
    vim.fn.jobwait({Idline})
-   local commandline = "lg " .. "@l_" .. filenameshort
+   local commandline = "lg " .. "/tmp/@l_" .. filenameshort
    vim.cmd(commandline)
 end
 
