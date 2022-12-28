c3202678c5cb277c53461de8b9cd501e68f3227f
yashamon <yasha.savelyev@gmail.com>
2022-12-27 20:33:57 -0600

    -a

--- config/nvimnew/@l_blah.lua
+++
@@ -1,3 +0,0 @@
- C:/Users/yasha/dotfiles/config/nvimnew/blah.lua:1:: local fzf = require('fzf')
- C:/Users/yasha/dotfiles/config/nvimnew/blah.lua:2:: 
- C:/Users/yasha/dotfiles/config/nvimnew/blah.lua:3:: coroutine.wrap(function() print(fzf.fzf({"1", "2", "3", "  preview 'bash   version'")) end)()
--- config/nvimnew/blah.lua
+++
@@ -1,3 +0,0 @@
-local fzf = require('fzf')
-
-coroutine.wrap(function() print(fzf.fzf({"1", "2", "3"}, "--preview 'bash --version'")) end)()
