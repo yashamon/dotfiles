9384946f3768f2998d780c1fb0d3e67533d9b3ee
yashamon <yasha.savelyev@gmail.com>
2022-12-27 19:39:35 -0600

    -a

--- config/nvimnew/lua/autocommand.lua
+++ config/nvimnew/lua/autocommand.lua
@@ -2,14 +2,6 @@ local au = vim.api.nvim_create_autocmd
 local ag = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
--- autocmd(
---     { "UIEnter"},
---     {
---       callback = function() CG()
--- 			end,
---       pattern = "markdown, txt",
---     }
--- )
 local timer = 0
 local function do_complete()
 	require('cmp').complete({ reason = require('cmp').ContextReason.Auto })
@@ -35,7 +27,6 @@ au('FileType', {pattern = 'vim', command = 'vim.opt.list = true'})
 au('FileType', {pattern = 'tex', command = 'vim.opt.wrapmargin=0'})
 au('BufWritePost', { callback = function() GitAsync() end })
 au('FileType', {pattern = 'tex, lua, vim', callback = function() CG() end })
-
 au('FileType', { pattern = 'tex', command = 'vmap q xi<CR><CR><CR><CR><ESC>kki/begin{comment}<cr><cr>/end{comment}<esc>kp'})
 local yankGrp = ag("YankHighlight", { clear = true })
 au("TextYankPost", {
