1bd6aba2992944ca38153baa593d2252226a8174
yashamon <yasha.savelyev@gmail.com>
2022-12-30 21:50:02 -0600

    -a

---
+++ config/nvimnew/lua/lines.lua
@@ -0,0 +1,43 @@
+Filename = vim.fn.substitute(vim.fn.expand('%:p'), "\\", "/", "g")
+GetFilename = function(path)
+    local start, finish = path:find('[%w%s!-={-|]+[_%.].+')
+    return path:sub(start,#path)
+end
+local basename = GetFilename(vim.fn.bufname())
+Tempfile = '/tmp' .. '/@l_' .. basename
+ToList = function ()
+	local newlines = {}
+	local i = 1
+	for line in io.lines(Filename) do
+		-- line = line:gsub("%^a", "")
+		newlines[i] = basename .. ":" .. "i" .. " " .. line
+		i = i + 1
+	end
+	return newlines
+end
+-- print(LinesLua())
+ToString = function(array)
+	local string = ""
+	local n = 1
+	-- string = string .. '\n' .. array[n]	
+	for i,v in ipairs(array) do
+		string = string .. v .. '\n'
+	end
+	print (string)
+	return string
+end
+-- ToString = function (stack)
+-- 	ToString({}) = ""
+-- 	return toString (stack.next) .. stack.line
+-- local string = ""
+-- 	for lines in stack do
+-- 		string = string .. stack.line
+-- 	while not stack.next == {} do
+-- 		string = 
+-- 	else return 
+-- 	end
+-- end
+LinesLua = function ()
+	io.open(Tempfile, 'w')
+	io.write(ToString(ToList()))
+end
