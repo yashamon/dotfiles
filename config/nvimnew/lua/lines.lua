a09d549a43dfe42e24b1d5b0a1c0572629085de4
yashamon <yasha.savelyev@gmail.com>
2022-12-31 00:56:09 -0600

    -a

--- config/nvimnew/lua/lines.lua
+++ config/nvimnew/lua/lines.lua
@@ -11,7 +11,7 @@ local toList = function ()
 	local i = 1
 	for line in io.lines(Filename) do
 		-- line = line:gsub("%^a", "")
-		newlines[i] = Filename .. ":" .. "i" .. " " .. line
+		newlines[i] = Filename .. ":" .. "i" .. " " .. line .. "\r\n"
 		i = i + 1
 	end
 	return newlines
@@ -21,7 +21,7 @@ WriteLine = function()
 	Temp = io.open(Tempfile, 'r')
 	-- string = string .. '\n' .. array[n]	
 	for i,v in ipairs(toList()) do
-		Temp:write(v, "\r\n")
+		Temp:write(v)
 	end
 	Temp:close()
 end
