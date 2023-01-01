f2d5396a48b19f77a10d5aafcd1d339a6e04a9a6
yashamon <yasha.savelyev@gmail.com>
2023-01-01 11:53:39 -0600

    -a

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,7 +1,7 @@
 local handle = io.popen("fd . -H -I -d 1 | fzf")
 local result = handle:read("*a")
 handle:close()
-lua print(result)
+print(result)
 -- local res = io.popen (, 'r')
 -- local res = "blah"
 -- os.execute('echo ' .. res )
