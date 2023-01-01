02689a3573a53eb233ac52e67eb01bf5cbb895dc
yashamon <yasha.savelyev@gmail.com>
2023-01-01 13:51:03 -0600

    -a

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,7 +1,7 @@
 local handle = io.popen("fd . -H -I -d 1 | fzf")
-local result = handle:read("*a")
+local res= handle:read("*a")
 handle:close()
-print(result)
+-- if  res | path type == dir  { lf -remote $"send cd \"($res)\"" } else { lf -remote $"send select \"($res)\"" }
 -- local res = io.popen (, 'r')
 -- local res = "blah"
 -- os.execute('echo ' .. res )
