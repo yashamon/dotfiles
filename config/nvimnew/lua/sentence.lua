026a670eb509cc5f2c761b23e35fc9f309179e32
yashamon <yasha.savelyev@gmail.com>
2023-01-02 12:12:38 -0600

    -a

--- config/nvimnew/lua/sentence.lua
+++ config/nvimnew/lua/sentence.lua
@@ -23,7 +23,7 @@ local tempfile = 'C:\\tmp\\@s_' .. filenameshort
 		string = string .. ' ' .. v
 	end
 	Temp = io.open(tempfile, 'w')
-	Temp:write(Newlines[2])
+	Temp:write(string)
 	Temp:close()
 end
 
