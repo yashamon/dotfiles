0c60212e7861fcd304063f735a0769c074607142
yashamon <yasha.savelyev@gmail.com>
2023-01-09 20:02:54 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -254,7 +254,9 @@ undercurl = true,
         -- Statement = { fg = "#0000ff" }
     }
 })
-
+require("yanker").config({
+  history = "<leader>yh",
+})
 -- nvim LSP
 require('snippets')
 require("mason").setup()
