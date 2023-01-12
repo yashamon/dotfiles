9a474a9b6d4047fb592ca532614020df4f56f324
yashamon <yasha.savelyev@gmail.com>
2023-01-12 10:39:30 -0600

    -a

--- scripts/lfcdextract.ps1
+++ scripts/lfcdextract.ps1
@@ -4,7 +4,7 @@ switch (Split-Path $f -Extension)
         ("tar.bz" -or "tar.bz2" -or "tbz" -or "tbz2")  {tar xjvf $f}
         ("tar.gz" -or "tgz") {tar xzvf $f}
         ("tar.xz" -or "txz") {tar xJvf $f}
-        ".zip" {unzip $f}
+        ".zip" {7z x $f}
         ".rar" {unrar x $f}
         ".7z" {7z x $f}
 }    
