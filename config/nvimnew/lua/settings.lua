15523ba6291fc62b7a8b67c2c33ad151d8a9ad91
yashamon <yasha.savelyev@gmail.com>
2023-01-14 19:39:11 -0600

    -a

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -165,7 +165,7 @@ on_highlights = function(highlights, colors)
 		highlights.DiagnosticSignHint = { bg = "#ffa546" }
 		highlights.texType = { fg = "#808080" }
 		highlights.texMath = { fg = "#650000" }
-		highlights.["@constructor"] = { fg = "#650000", style = {} }
+		highlights.@constructor = { fg = "#650000", style = {} }
 		highlights.TexSpecial = { fg = "#af6000" }
 		highlights.texDelimiter = { fg = "#af6000" }
 		highlights.texStatement = { fg = "#580058" }
