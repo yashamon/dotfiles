60c7c85f56ab058fbfce76c31250bf31be4411c8
yashamon <yasha.savelyev@gmail.com>
2023-05-24 14:26:23 -0600

    -a

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -76,7 +76,7 @@ map('n','o', 'o<space><esc>"_s', {})
 map('n', '<m-q>', function () vim.cmd('wq') end, {})
 map('n', '<m-Q>', function () vim.cmd('q') end, {})
 map('n', '<m-c>', function () vim.cmd('close') end, {})
-map('n', '<m-d>', function () vim.cmd('up | bdelete') end, {})
+map('n', '<c-d>', function () vim.cmd('up | bdelete') end, {})
 map('n', '<m-D>', function () vim.cmd('up | bdelete!') end, {})
 map('n', '<leader>r', function () vim.cmd('up | e') end, {})
 
