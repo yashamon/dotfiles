1211379b41924427a814a939c8e5625d043b2c4c
yashamon <yasha.savelyev@gmail.com>
2023-05-26 21:06:39 -0600

    -a

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -105,7 +105,7 @@ map ('i', '\\', '/')
 map ('i', '/',  '\\')
 map ('i', '<m-d>', '<C-w>')
 map ('i', '<m-p>', '<c-r>+')
-map ('n', '<c-L>', 'viwy<leader>e:<c-r>+<cr>', {remap = true})
+map ('n', '<c-E>', 'viwy<leader>e:<c-r>+<cr>', {remap = true})
 
 vim.keymap.set("c", "<cr>", function()
   if vim.fn.pumvisible() == 1 then return '<c-y>' end
