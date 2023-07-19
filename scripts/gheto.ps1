e62b49dde8be0931b51de08b7ab2733691813b64
yashamon <yasha.savelyev@gmail.com>
2023-07-19 15:32:43 +0100

    -a # Differences to be committed: #	diff --git a/scripts/gheto.ps1 b/scripts/gheto.ps1 #	index db2305a13..8266c8a34 100644 #	--- a/scripts/gheto.ps1 #	+++ b/scripts/gheto.ps1 #	@@ -1,4 +1,5 @@ #	 $HOMED="$HOME//OneDrive" #	+$olddir="$HOME//dotfiles_old" #	 nu -c "mv $HOME//scoop//apps/summatrapdf//current//SummatraPDF-settings.txt $olddir" #	 nu -c "sudo ln -s $HOMED//dotfile//config//sumatra//SumatraPDF-settings.txt $HOME//scoop//apps//sumatrapdf//current//SumatraPDF-settings.txt" #

--- scripts/gheto.ps1
+++ scripts/gheto.ps1
@@ -1,4 +1,5 @@
 $HOMED="$HOME//OneDrive"
+$olddir="$HOME//dotfiles_old"
 nu -c "mv $HOME//scoop//apps/summatrapdf//current//SummatraPDF-settings.txt $olddir"
 nu -c "sudo ln -s $HOMED//dotfile//config//sumatra//SumatraPDF-settings.txt $HOME//scoop//apps//sumatrapdf//current//SumatraPDF-settings.txt"
 
