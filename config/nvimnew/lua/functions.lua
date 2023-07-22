7ce0d22d57bbc7611004f2d1996004e3acd9b20c
yashamon <yasha.savelyev@gmail.com>
2023-07-22 17:29:33 +0100

    diff --git a/config/nvimnew/lua/functions.lua b/config/nvimnew/lua/functions.lua index 2f1e615a2..b8f253f7b 100644 --- a/config/nvimnew/lua/functions.lua +++ b/config/nvimnew/lua/functions.lua @@ -104,7 +104,7 @@ GitAsync = function()  -- local buff = vim.fn.bufname()  local commandsentence = 'if ((git rev-parse --is-inside-work-tree) | into bool ) {rm git.log; touch git.log; git add . | save --append git.log; git commit -m $"(git diff --staged)" | save --append git.log; git push --all origin} | save --append git.log'  Job(commandsentence) --- vim.cmd("buffer " .. buff) +-- vim.cmd("buffer " .. buff) test  end
     ViewPdf2 = function()

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -104,7 +104,7 @@ GitAsync = function()
 -- local buff = vim.fn.bufname()
 local commandsentence = 'if ((git rev-parse --is-inside-work-tree) | into bool ) {rm git.log; touch git.log; git add . | save --append git.log; git commit -m $"(git diff --staged)" | save --append git.log; git push --all origin} | save --append git.log'
 Job(commandsentence)
--- vim.cmd("buffer " .. buff) 
+-- vim.cmd("buffer " .. buff) test
 end
 
 ViewPdf2 = function()
