fde75e191d52389af8039c68ae009f0ea9c9430f
yashamon <yasha.savelyev@gmail.com>
2023-07-22 15:16:23 +0100

    diff --git a/config/nvimnew/lua/functions.lua b/config/nvimnew/lua/functions.lua index 561b87d22..d1b95590a 100644 --- a/config/nvimnew/lua/functions.lua +++ b/config/nvimnew/lua/functions.lua @@ -97,14 +97,13 @@ Job = function(string)  -- local buff = vim.fn.bufname()  local jobstring = 'call jobstart(\'' .. 'nu -c "' .. string .. '"\')'  vim.cmd(jobstring) -vim.cmd('echo "job start success"')  -- vim.cmd("buffer " .. buff) test  end
     GitAsync = function()
     -- local buff = vim.fn.bufname()
    -local commandsentence = 'if ((git rev-parse --is-inside-work-tree) | into bool ) { git add . | save --append git.log; git commit -m (git diff --staged) | save --append git.log; git push --all origin} | save --append git.log'
    -Job(commandsentence)
    +local commandsentence = 'if ((git rev-parse --is-inside-work-tree) | into bool ) {rm git.log; git add . | save --append git.log; git commit -m (git diff --staged) | save --append git.log; git push --all origin} | save --append git.log'
    +GitJobId = Job(commandsentence)
     -- vim.cmd("buffer " .. buff)
     end

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -97,14 +97,13 @@ Job = function(string)
 -- local buff = vim.fn.bufname()
 local jobstring = 'call jobstart(\'' .. 'nu -c "' .. string .. '"\')'
 vim.cmd(jobstring)
-vim.cmd('echo "job start success"')
 -- vim.cmd("buffer " .. buff) test
 end
 
 GitAsync = function()
 -- local buff = vim.fn.bufname()
-local commandsentence = 'if ((git rev-parse --is-inside-work-tree) | into bool ) { git add . | save --append git.log; git commit -m (git diff --staged) | save --append git.log; git push --all origin} | save --append git.log'
-Job(commandsentence)
+local commandsentence = 'if ((git rev-parse --is-inside-work-tree) | into bool ) {rm git.log; git add . | save --append git.log; git commit -m (git diff --staged) | save --append git.log; git push --all origin} | save --append git.log'
+GitJobId = Job(commandsentence)
 -- vim.cmd("buffer " .. buff) 
 end
 
