08f1d793971f9fe97895225dcff1cebdebef79ec
yashamon <yasha.savelyev@gmail.com>
2023-07-23 18:53:33 +0100

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index e7b87d9e3..a1771bdf5 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -446,7 +446,7 @@ requires = {      },  -- ... Your other configuration ...  sources = cmp.config.sources({ --- { name = 'tags', keyword_length = 4 }, +{ name = 'tags', keyword_length = 4 },  { name = 'nvim_lsp', keyword_length = 2 },  { name = 'luasnip', keyword_length = 2 },  -- { name = 'omni', keyword_length = 4},

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -446,7 +446,7 @@ requires = {
     },
 -- ... Your other configuration ...
 sources = cmp.config.sources({
--- { name = 'tags', keyword_length = 4 },
+{ name = 'tags', keyword_length = 4 },
 { name = 'nvim_lsp', keyword_length = 2 },
 { name = 'luasnip', keyword_length = 2 },
 -- { name = 'omni', keyword_length = 4},
