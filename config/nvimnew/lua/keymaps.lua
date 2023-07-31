b712db32c0f5e962b98029058ab00f0442534c32
yashamon <yasha.savelyev@gmail.com>
2023-07-31 09:01:41 -0600

    diff --git a/config/nvimnew/lua/keymaps.lua b/config/nvimnew/lua/keymaps.lua index 5741c0d9b..312f5fe2b 100644 --- a/config/nvimnew/lua/keymaps.lua +++ b/config/nvimnew/lua/keymaps.lua @@ -20,6 +20,7 @@ command('Trim', "lua MiniTrailspace.trim()", {})  command('Ctags', "lua Job('ctags ~/OneDrive/dotfiles/link.bib')", {})
     --Session
    +local resession = require('resession')
     vim.keymap.set('n', '<leader>ss', resession.save)
     vim.keymap.set('n', '<leader>sl', resession.load)
     vim.keymap.set('n', '<leader>sd', resession.delete)

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -20,6 +20,7 @@ command('Trim', "lua MiniTrailspace.trim()", {})
 command('Ctags', "lua Job('ctags ~/OneDrive/dotfiles/link.bib')", {})
 
 --Session
+local resession = require('resession')
 vim.keymap.set('n', '<leader>ss', resession.save)
 vim.keymap.set('n', '<leader>sl', resession.load)
 vim.keymap.set('n', '<leader>sd', resession.delete)
