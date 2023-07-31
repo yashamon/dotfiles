f139807f1db5f57182d103946d92b442960dd116
yashamon <yasha.savelyev@gmail.com>
2023-07-31 08:59:11 -0600

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index 059f6296e..e8a50ae96 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -54,7 +54,10 @@ require'nu'.setup{}  -- }  require("resession").setup({    -- Options for automatically saving sessions on a timer -	-- +-- Resession does NOTHING automagically, so we have to set up some keymaps +vim.keymap.set('n', '<leader>ss', resession.save) +vim.keymap.set('n', '<leader>sl', resession.load) +vim.keymap.set('n', '<leader>sd', resession.delete)    autosave = {      enabled = true,      -- How often to save (in seconds)

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -54,7 +54,10 @@ require'nu'.setup{}
 -- }
 require("resession").setup({
   -- Options for automatically saving sessions on a timer
-	-- 
+-- Resession does NOTHING automagically, so we have to set up some keymaps
+vim.keymap.set('n', '<leader>ss', resession.save)
+vim.keymap.set('n', '<leader>sl', resession.load)
+vim.keymap.set('n', '<leader>sd', resession.delete)
   autosave = {
     enabled = true,
     -- How often to save (in seconds)
