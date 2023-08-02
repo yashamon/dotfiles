60b1c653b2bdd8956114e7348dbccff9954b5bbb
yashamon <yasha.savelyev@gmail.com>
2023-08-01 18:40:44 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index 8f484fd45..7238e3cff 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -4,7 +4,7 @@ $code =  @"  var options = new Windows.System.LauncherOptions();  options.DisplayApplicationPicker = true;
    -bool success = await Windows.System.Launcher.LaunchFileAsync($file, "nvim");
    +bool success = await Windows.System.Launcher.LaunchFileAsync($file, "goneovim");
     "@
     Add-Type -TypeDefinition $code -Language CSharp
     iex "[openfile.Program]::Main()"

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -4,7 +4,7 @@ $code =  @"
 var options = new Windows.System.LauncherOptions();
 options.DisplayApplicationPicker = true;
 
-bool success = await Windows.System.Launcher.LaunchFileAsync($file, "nvim");
+bool success = await Windows.System.Launcher.LaunchFileAsync($file, "goneovim");
 "@
 Add-Type -TypeDefinition $code -Language CSharp
 iex "[openfile.Program]::Main()"
