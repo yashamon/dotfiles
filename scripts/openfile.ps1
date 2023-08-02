d8aa1d56cfdd34c65f43a21c10fdbd574c65c2ab
yashamon <yasha.savelyev@gmail.com>
2023-08-01 18:38:39 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index 60396c754..37f610380 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -1,9 +1,9 @@ - +$file = $args[0]  $code =  @"  var options = new Windows.System.LauncherOptions();  options.DisplayApplicationPicker = true;
    -bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
    +bool success = await Windows.System.Launcher.LaunchFileAsync($file, options);
     "@
     Add-Type -TypeDefinition $code -Language CSharp
     iex "[openfile.Program]::Main()"

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -1,9 +1,9 @@
-
+$file = $args[0]
 $code =  @"
 var options = new Windows.System.LauncherOptions();
 options.DisplayApplicationPicker = true;
 
-bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
+bool success = await Windows.System.Launcher.LaunchFileAsync($file, options);
 "@
 Add-Type -TypeDefinition $code -Language CSharp
 iex "[openfile.Program]::Main()"
