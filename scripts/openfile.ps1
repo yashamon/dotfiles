8f2fb23e966238b0e32da1aa3c1685434f9e411d
yashamon <yasha.savelyev@gmail.com>
2023-08-01 18:45:40 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index 6ae8d669f..cd51119f7 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -1,11 +1,10 @@  $file = $args[0] -echo $file  $code =  @" -import Windows +using Windows;  var options = new Windows.System.LauncherOptions();  options.DisplayApplicationPicker = true;
    -bool success = await Windows.System.Launcher.LaunchFileAsync($file, "goneovim");
    +bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
     "@
     Add-Type -TypeDefinition $code -Language CSharp
     iex "[openfile.Program]::Main()"

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -1,11 +1,10 @@
 $file = $args[0]
-echo $file
 $code =  @"
-import Windows
+using Windows;
 var options = new Windows.System.LauncherOptions();
 options.DisplayApplicationPicker = true;
 
-bool success = await Windows.System.Launcher.LaunchFileAsync($file, "goneovim");
+bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
 "@
 Add-Type -TypeDefinition $code -Language CSharp
 iex "[openfile.Program]::Main()"
