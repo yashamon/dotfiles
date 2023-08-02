e1c4856205124e70fd77b890147813b77f8e886f
yashamon <yasha.savelyev@gmail.com>
2023-08-01 18:49:04 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index cd600749a..7c8d2837d 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -4,7 +4,7 @@ using System.Windows.Forms;  var options = new Windows.System.LauncherOptions();  options.DisplayApplicationPicker = true;
    -bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
    +bool success = await Windows.System.Launcher.LaunchFileAsync($file, options);
     "@
     Add-Type -TypeDefinition $code -Language CSharp
     iex "[openfile.Program]::Main()"

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 var options = new Windows.System.LauncherOptions();
 options.DisplayApplicationPicker = true;
 
-bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
+bool success = await Windows.System.Launcher.LaunchFileAsync($file, options);
 "@
 Add-Type -TypeDefinition $code -Language CSharp
 iex "[openfile.Program]::Main()"
