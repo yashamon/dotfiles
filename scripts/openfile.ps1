be431caa1f6520d90734c7e5b96fd89d2e641b30
yashamon <yasha.savelyev@gmail.com>
2023-08-01 20:01:13 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index 79348f0c5..2b94c3a90 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -3,4 +3,4 @@ $file = $args[0]  $options = New-Object -TypeName new Windows.System.LauncherOptions  $options.DisplayApplicationPicker = true
    -$success = New-Object -TypeName await Windows.System.Launcher.LaunchFileAsync($file, options)
    +$success = New-Object -TypeName await Windows.System.Launcher.LaunchFileAsync($file, $options)

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -3,4 +3,4 @@ $file = $args[0]
 $options = New-Object -TypeName new Windows.System.LauncherOptions
 $options.DisplayApplicationPicker = true
 
-$success = New-Object -TypeName await Windows.System.Launcher.LaunchFileAsync($file, options)
+$success = New-Object -TypeName await Windows.System.Launcher.LaunchFileAsync($file, $options)
