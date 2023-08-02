916a638f426f9fcb79d2100fcf34a2f33257a82d
yashamon <yasha.savelyev@gmail.com>
2023-08-01 20:07:54 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index ba0bed59c..be7589ca4 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -1,6 +1,29 @@ -$file = $args[0] +async void DefaultLaunch() +{ +   // Path to the file in the app package to launch +      string imageFile = @"images\test.png"; + +   var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(imageFile);
    -$options = New-Object -TypeName Windows.System.LauncherOptions
    -$options.DisplayApplicationPicker = "true"
    +   if (file != null)
    +   {
    +      // Set the option to show the picker
    +      var options = new Windows.System.LauncherOptions();
    +      options.DisplayApplicationPicker = true;
    
    -$success = New-Object -TypeName await Windows.System.Launcher.LaunchFileAsync($file, $options)
    +      // Launch the retrieved file
    +      bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
    +      if (success)
    +      {
    +         // File launched
    +      }
    +      else
    +      {
    +         // File launch failed
    +      }
    +   }
    +   else
    +   {
    +      // Could not find file
    +   }
    +}

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -1,6 +1,29 @@
-$file = $args[0]
+async void DefaultLaunch()
+{
+   // Path to the file in the app package to launch
+      string imageFile = @"images\test.png";
+      
+   var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(imageFile);
 
-$options = New-Object -TypeName Windows.System.LauncherOptions
-$options.DisplayApplicationPicker = "true"
+   if (file != null)
+   {
+      // Set the option to show the picker
+      var options = new Windows.System.LauncherOptions();
+      options.DisplayApplicationPicker = true;
 
-$success = New-Object -TypeName await Windows.System.Launcher.LaunchFileAsync($file, $options)
+      // Launch the retrieved file
+      bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
+      if (success)
+      {
+         // File launched
+      }
+      else
+      {
+         // File launch failed
+      }
+   }
+   else
+   {
+      // Could not find file
+   }
+}
