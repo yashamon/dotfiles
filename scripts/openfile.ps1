71de10a2fb44a2d5f857304f341e1fab3c82223d
yashamon <yasha.savelyev@gmail.com>
2023-08-01 18:37:11 -0600

    diff --git a/scripts/openfile.ps1 b/scripts/openfile.ps1 index 3b82f9e20..60396c754 100644 --- a/scripts/openfile.ps1 +++ b/scripts/openfile.ps1 @@ -1,33 +1,9 @@
     $code =  @"
    -async void DefaultLaunch()
    -{
    -   // Path to the file in the app package to launch
    -      string imageFile = @"images\test.png";
    -
    -   var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(imageFile);
    +var options = new Windows.System.LauncherOptions();
    +options.DisplayApplicationPicker = true;
    
    -   if (file != null)
    -   {
    -      // Set the option to show the picker
    -      var options = new Windows.System.LauncherOptions();
    -      options.DisplayApplicationPicker = true;
    -
    -      // Launch the retrieved file
    -      bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
    -      if (success)
    -      {
    -         // File launched
    -      }
    -      else
    -      {
    -         // File launch failed
    -      }
    -   }
    -   else
    -   {
    -      // Could not find file
    -   }
    -}"@
    +bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
    +"@
     Add-Type -TypeDefinition $code -Language CSharp
     iex "[openfile.Program]::Main()"

--- scripts/openfile.ps1
+++ scripts/openfile.ps1
@@ -1,33 +1,9 @@
 
 $code =  @"
-async void DefaultLaunch()
-{
-   // Path to the file in the app package to launch
-      string imageFile = @"images\test.png";
-      
-   var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(imageFile);
+var options = new Windows.System.LauncherOptions();
+options.DisplayApplicationPicker = true;
 
-   if (file != null)
-   {
-      // Set the option to show the picker
-      var options = new Windows.System.LauncherOptions();
-      options.DisplayApplicationPicker = true;
-
-      // Launch the retrieved file
-      bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
-      if (success)
-      {
-         // File launched
-      }
-      else
-      {
-         // File launch failed
-      }
-   }
-   else
-   {
-      // Could not find file
-   }
-}"@
+bool success = await Windows.System.Launcher.LaunchFileAsync(file, options);
+"@
 Add-Type -TypeDefinition $code -Language CSharp
 iex "[openfile.Program]::Main()"
