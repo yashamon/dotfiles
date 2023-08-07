6ff1db0bc4c3e8bf24a31c5d84295cc54f2554f3
yashamon <yasha.savelyev@gmail.com>
2023-08-07 11:53:17 -0600

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index bbe12f0c5..ba9c8cca4 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -126,7 +126,7 @@ require('mini.surround').setup(    -- Module mappings. Use `''` (empty string) to disable one.    mappings = {      add = ';sa', --Add surrounding in Normal and Visual modes -    delete = ';sd', -- Delete surrounding +    -- delete = ';sd', -- Delete surrounding      find = ';sf', -- Find surrounding (to the right)      find_left = ';sF', -- Find surrounding (to the left)      highlight = ';sh', -- Highlight surrounding

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -126,7 +126,7 @@ require('mini.surround').setup(
   -- Module mappings. Use `''` (empty string) to disable one.
   mappings = {
     add = ';sa', --Add surrounding in Normal and Visual modes
-    delete = ';sd', -- Delete surrounding
+    -- delete = ';sd', -- Delete surrounding
     find = ';sf', -- Find surrounding (to the right)
     find_left = ';sF', -- Find surrounding (to the left)
     highlight = ';sh', -- Highlight surrounding
