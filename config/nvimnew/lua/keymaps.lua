3a736716088313a30d6120dc978422a4cd6b5985
yashamon <yasha.savelyev@gmail.com>
2023-08-24 20:08:29 -0600

    diff --git a/config/nvimnew/lua/keymaps.lua b/config/nvimnew/lua/keymaps.lua index 753fe8e75..7533490ec 100644 --- a/config/nvimnew/lua/keymaps.lua +++ b/config/nvimnew/lua/keymaps.lua @@ -6,10 +6,12 @@ map('t', '<m-p>', [[getreg('+')]], {expr = true})  map({'i','n'}, '<m-t>', function ()  vim.cmd('up')  end, { buffer=true })  map('n','S', Line, {} ) -map('n','M', function() require("harpoon.mark").add_file() end, {}) -map('n','<Tab>', function() require("harpoon.ui").nav_next() end,{}) -map('n','<S-Tab>', function() require("harpoon.ui").nav_prev() end, {}) -map('n',';hl', function() require("harpoon.ui").toggle_quick_menu() end, {}) +map('n','Tab', function() vim.cmd('edit #') end, {} ) + +-- map('n','M', function() require("harpoon.mark").add_file() end, {}) +-- map('n','<Tab>', function() require("harpoon.ui").nav_next() end,{}) +-- map('n','<S-Tab>', function() require("harpoon.ui").nav_prev() end, {}) +-- map('n',';hl', function() require("harpoon.ui").toggle_quick_menu() end, {})  map('n', '<m-s>', Sentence, {})

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -6,10 +6,12 @@ map('t', '<m-p>', [[getreg('+')]], {expr = true})
 map({'i','n'}, '<m-t>', function ()  vim.cmd('up')
 end, { buffer=true })
 map('n','S', Line, {} )
-map('n','M', function() require("harpoon.mark").add_file() end, {})
-map('n','<Tab>', function() require("harpoon.ui").nav_next() end,{})
-map('n','<S-Tab>', function() require("harpoon.ui").nav_prev() end, {})
-map('n',';hl', function() require("harpoon.ui").toggle_quick_menu() end, {})
+map('n','Tab', function() vim.cmd('edit #') end, {} )
+
+-- map('n','M', function() require("harpoon.mark").add_file() end, {})
+-- map('n','<Tab>', function() require("harpoon.ui").nav_next() end,{})
+-- map('n','<S-Tab>', function() require("harpoon.ui").nav_prev() end, {})
+-- map('n',';hl', function() require("harpoon.ui").toggle_quick_menu() end, {})
 map('n', '<m-s>', Sentence, {})
 
 
