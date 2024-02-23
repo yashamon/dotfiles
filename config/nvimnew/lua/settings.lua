2a1f0c3b2a596b69d97084aa058786d785849d14
yashamon <yasha.savelyev@gmail.com>
2024-02-23 18:48:33 -0500

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index 290259a8c..c899aa5e5 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -33,6 +33,25 @@ ts_config.setup {  	autotag = {enable = true},  	rainbow = {enable = true},  } +require("noice").setup({ +  lsp = { +    -- override markdown rendering so that **cmp** and other plugins use **Treesitter** +    override = { +      ["vim.lsp.util.convert_input_to_markdown_lines"] = true, +      ["vim.lsp.util.stylize_markdown"] = true, +      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp +    }, +  }, +  -- you can enable a preset for easier configuration +  presets = { +    bottom_search = true, -- use a classic bottom cmdline for search +    command_palette = true, -- position the cmdline and popupmenu together +    long_message_to_split = true, -- long messages will be sent to a split +    inc_rename = false, -- enables an input dialog for inc-rename.nvim +    lsp_doc_border = false, -- add a border to hover docs and signature help +  }, +}) +  require'nu'.setup{}  -- require'nvim-treesitter.configs'.setup {  -- -- ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -33,6 +33,25 @@ ts_config.setup {
 	autotag = {enable = true},
 	rainbow = {enable = true},
 }
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
+
 require'nu'.setup{}
 -- require'nvim-treesitter.configs'.setup {
 -- -- ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
