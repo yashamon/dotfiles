1294e9b264c8f2da25c2ac5ddf685903304a3bd7
yashamon <yasha.savelyev@gmail.com>
2024-04-16 10:50:23 -0400

    diff --git a/config/nvimnew/lua/keymaps.lua b/config/nvimnew/lua/keymaps.lua index a02492477..022d51bca 100644 --- a/config/nvimnew/lua/keymaps.lua +++ b/config/nvimnew/lua/keymaps.lua @@ -236,8 +236,8 @@ nnoremap <Backspace> i<Backspace><Esc>  noremap <A-r> <C-r>  nnoremap ` ~  nnoremap . ` -noremap <m-o> <C-o> -noremap <m-i> <C-i> +noremap <C-o> <esc><C-o> +noremap <C-i> <esc><C-i>  noremap <D-u> <C-u>  noremap <A-u> <C-u>  ]])

--- config/nvimnew/lua/keymaps.lua
+++ config/nvimnew/lua/keymaps.lua
@@ -236,8 +236,8 @@ nnoremap <Backspace> i<Backspace><Esc>
 noremap <A-r> <C-r>
 nnoremap ` ~
 nnoremap . `
-noremap <m-o> <C-o>
-noremap <m-i> <C-i>
+noremap <C-o> <esc><C-o>
+noremap <C-i> <esc><C-i>
 noremap <D-u> <C-u>
 noremap <A-u> <C-u>
 ]])
