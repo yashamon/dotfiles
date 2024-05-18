93688d2702e54101eb4c62d65523f9adf27da2b5
yashamon <yasha.savelyev@gmail.com>
2024-05-18 12:10:35 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index bc34cb2d1..ec30bc219 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -115,11 +115,11 @@ end,},
     })
     -- luasnip
    -require("luasnip/loaders/from_vscode").lazy_load({ paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",  fs_event_providers = {
    +require("luasnip/loaders/from_vscode").load({ paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",  fs_event_providers = {
                         autocmd = true,
                         libuv = true}
     })
    -require("luasnip.loaders.from_lua").lazy_load({
    +require("luasnip.loaders.from_lua").load({
                     paths = "C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets",
                     fs_event_providers = {
                         autocmd = true,
    diff --git a/config/nvimnew/lua/snippets/blah.tex b/config/nvimnew/lua/snippets/blah.tex
    index 40e0a6f54..8b1378917 100644
    --- a/config/nvimnew/lua/snippets/blah.tex
    +++ b/config/nvimnew/lua/snippets/blah.tex
    @@ -1 +1 @@
    -letter
    +

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -115,11 +115,11 @@ end,},
 
 })
 -- luasnip
-require("luasnip/loaders/from_vscode").lazy_load({ paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",  fs_event_providers = {
+require("luasnip/loaders/from_vscode").load({ paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",  fs_event_providers = {
                     autocmd = true,
                     libuv = true}
 })
-require("luasnip.loaders.from_lua").lazy_load({
+require("luasnip.loaders.from_lua").load({
                 paths = "C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets",
                 fs_event_providers = {
                     autocmd = true,
