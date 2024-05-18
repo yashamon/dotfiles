3c65bc7608adff20e6592274e8db697221e418a7
yashamon <yasha.savelyev@gmail.com>
2024-05-18 12:36:39 -0400

    diff --git a/config/nvimnew/lua/snippets/all.lua b/config/nvimnew/lua/snippets/all.lua index 21b3923fb..57564e501 100644 --- a/config/nvimnew/lua/snippets/all.lua +++ b/config/nvimnew/lua/snippets/all.lua @@ -391,7 +391,7 @@ ls.add_snippets("tex", {  })  ls.add_snippets("tex", {  	s("footnote", { -		t({"\\footnote{"}), i(1, "ten"), t({"}"}), +		t({"\\footnote{"}), i(1, "tengu"), t({"}"}),  	}, { condition = not Is_math }),  })  ls.add_snippets("tex", {

--- config/nvimnew/lua/snippets/all.lua
+++ config/nvimnew/lua/snippets/all.lua
@@ -391,7 +391,7 @@ ls.add_snippets("tex", {
 })
 ls.add_snippets("tex", {
 	s("footnote", {
-		t({"\\footnote{"}), i(1, "ten"), t({"}"}),
+		t({"\\footnote{"}), i(1, "tengu"), t({"}"}),
 	}, { condition = not Is_math }),
 })
 ls.add_snippets("tex", {
