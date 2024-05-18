5e7cc6a51f7fdd33649025657aaf02276d8fce1d
yashamon <yasha.savelyev@gmail.com>
2024-05-18 12:08:52 -0400

    diff --git a/config/nvimnew/lua/snippets/blah.tex b/config/nvimnew/lua/snippets/blah.tex new file mode 100644 index 000000000..40e0a6f54 --- /dev/null +++ b/config/nvimnew/lua/snippets/blah.tex @@ -0,0 +1 @@ +letter diff --git a/config/nvimnew/lua/snippets/snippets.lua b/config/nvimnew/lua/snippets/snippets.lua index 9b1abc559..b61df8fc1 100644 --- a/config/nvimnew/lua/snippets/snippets.lua +++ b/config/nvimnew/lua/snippets/snippets.lua @@ -718,16 +718,5 @@ t({"","\\end{document}"}),  -- Mind that this will extend  `ls.snippets` so you need to do it after your own snippets or you  -- will need to extend the table yourself instead of setting a new one.  ]] -require("luasnip/loaders/from_vscode").lazy_load({ paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",  fs_event_providers = { -                    autocmd = true, -                    libuv = true} -}) -require("luasnip.loaders.from_lua").lazy_load({ -                paths = "C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets", -                fs_event_providers = { -                    autocmd = true, -                    libuv = true, -                }, -            })
     -- You can also use lazy loading so you only get in memory snippets of languages you use

--- config/nvimnew/lua/snippets/snippets.lua
+++ config/nvimnew/lua/snippets/snippets.lua
@@ -718,16 +718,5 @@ t({"","\\end{document}"}),
 -- Mind that this will extend  `ls.snippets` so you need to do it after your own snippets or you
 -- will need to extend the table yourself instead of setting a new one.
 ]]
-require("luasnip/loaders/from_vscode").lazy_load({ paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",  fs_event_providers = {
-                    autocmd = true,
-                    libuv = true}
-})
-require("luasnip.loaders.from_lua").lazy_load({
-                paths = "C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets",
-                fs_event_providers = {
-                    autocmd = true,
-                    libuv = true,
-                },
-            })
 
 -- You can also use lazy loading so you only get in memory snippets of languages you use
