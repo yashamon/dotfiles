fab88907c218cef69492f656b69efa0eb3bd3d4a
yashamon <yasha.savelyev@gmail.com>
2024-05-18 12:11:43 -0400

    diff --git a/config/nvimnew/lua/snippets/snippets.lua b/config/nvimnew/lua/snippets/snippets.lua index b64e3484b..09dab7f6b 100644 --- a/config/nvimnew/lua/snippets/snippets.lua +++ b/config/nvimnew/lua/snippets/snippets.lua @@ -391,7 +391,7 @@ ls.add_snippets("tex", {  })  ls.add_snippets("tex", {  	s("footnote", { -		t({"\\footnote{"}), i(1, ""), t({"}"}), +		t({"\\footnote{"}), i(1, "testi"), t({"}"}),  	}, { condition = not Is_math }),  })  ls.add_snippets("tex", {

--- config/nvimnew/lua/snippets/snippets.lua
+++ config/nvimnew/lua/snippets/snippets.lua
@@ -391,7 +391,7 @@ ls.add_snippets("tex", {
 })
 ls.add_snippets("tex", {
 	s("footnote", {
-		t({"\\footnote{"}), i(1, ""), t({"}"}),
+		t({"\\footnote{"}), i(1, "testi"), t({"}"}),
 	}, { condition = not Is_math }),
 })
 ls.add_snippets("tex", {
