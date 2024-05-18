25e9cb08d71f82abe9d3a2f89c10598951bd8efa
yashamon <yasha.savelyev@gmail.com>
2024-05-18 13:53:59 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 620098050..66e7428eb 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -83,21 +83,21 @@ require("lazy").setup({  {'justinhoward/fzf-neoyank', lazy=false },  {'folke/tokyonight.nvim', lazy = true  }, -{ -  "folke/noice.nvim", -  event = "VeryLazy", -  opts = { -    -- add any options here -  }, -  dependencies = { -    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries -    "MunifTanjim/nui.nvim", -    -- OPTIONAL: -    --   `nvim-notify` is only needed, if you want to use the notification view. -    --   If not available, we use `mini` as the fallback -    -- "rcarriga/nvim-notify", -    } -}, +-- { +--   "folke/noice.nvim", +--   event = "VeryLazy", +--   opts = { +--     -- add any options here +--   }, +--   dependencies = { +--     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries +--     "MunifTanjim/nui.nvim", +--     -- OPTIONAL: +--     --   `nvim-notify` is only needed, if you want to use the notification view. +--     --   If not available, we use `mini` as the fallback +--     -- "rcarriga/nvim-notify", +--     } +-- },  {'ellisonleao/gruvbox.nvim',  lazy = false, priority = 1000,  config = function()

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -83,21 +83,21 @@ require("lazy").setup({
 {'justinhoward/fzf-neoyank', lazy=false },
 {'folke/tokyonight.nvim', lazy = true
 },
-{
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    -- "rcarriga/nvim-notify",
-    }
-},
+-- {
+--   "folke/noice.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     -- add any options here
+--   },
+--   dependencies = {
+--     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+--     "MunifTanjim/nui.nvim",
+--     -- OPTIONAL:
+--     --   `nvim-notify` is only needed, if you want to use the notification view.
+--     --   If not available, we use `mini` as the fallback
+--     -- "rcarriga/nvim-notify",
+--     }
+-- },
 {'ellisonleao/gruvbox.nvim',
 lazy = false, priority = 1000,
 config = function()
