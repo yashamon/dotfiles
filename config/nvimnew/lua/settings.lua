04512253349b98e82526dab597f890ae3d96f725
yashamon <yasha.savelyev@gmail.com>
2024-05-19 17:07:06 -0400

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index d396b3924..38aa1d099 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -476,15 +476,12 @@ require("mason-lspconfig").setup()  require("lspconfig")  -- LSP config  local lspconfig = require("lspconfig") - -    lspconfig.lua_ls.setup { on_attach = on_attach } -    lspconfig.tsserver.setup { on_attach = on_attach } -    lspconfig.jsonls.setup { on_attach = on_attach } -    lspconfig.rome.setup { on_attach = on_attach } -    lspconfig.vimls.setup { on_attach = on_attach } -    lspconfig.ltex.setup { autostart = false; -       on_attach = on_attach } -    lspconfig.texlab.setup { on_attach = on_attach } +lspconfig.tsserver.setup { on_attach = on_attach } +lspconfig.jsonls.setup { on_attach = on_attach } +lspconfig.rome.setup { on_attach = on_attach } +lspconfig.vimls.setup { on_attach = on_attach } +lspconfig.ltex.setup { autostart = false; on_attach = on_attach } +lspconfig.texlab.setup { on_attach = on_attach }  -- lspconfig stuff  -- local nvim_lsp = require('lspconfig')  -- Use an on_attach function to only map the following keys @@ -495,8 +492,39 @@ local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end  -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end  -- Enable completion triggered by <c-x><c-o>  buf_set_keymap('omnifunc', 'v:lua.vim.lsp.omnifunc') -require'lspconfig'.sumneko_lua.setup(require("config.lua-lsp")) -require'lspconfig'.lua_ls.setup(require("lualsp")) -- Mappings. +require'lspconfig'.lua_ls.setup { +  on_init = function(client) +    local path = client.workspace_folders[1].name +    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then +      return +    end + +    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, { +      runtime = { +        -- Tell the language server which version of Lua you're using +        -- (most likely LuaJIT in the case of Neovim) +        version = 'LuaJIT' +      }, +      -- Make the server aware of Neovim runtime files +      workspace = { +        checkThirdParty = false, +        library = { +          vim.env.VIMRUNTIME +          -- Depending on the usage, you might want to add additional paths here. +          -- "${3rd}/luv/library" +          -- "${3rd}/busted/library", +        } +        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower +        -- library = vim.api.nvim_get_runtime_file("", true) +      } +    }) +  end, +  settings = { +    Lua = {} +  } +} +-- require'lspconfig'.sumneko_lua.setup(require("config.lua-lsp")) +-- require'lspconfig'.lua_ls.setup(require("lualsp")) -- Mappings.  local opts = { noremap=true, silent=true }
       -- See `:help vim.lsp.*` for documentation on any of the below functions

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -476,15 +476,12 @@ require("mason-lspconfig").setup()
 require("lspconfig")
 -- LSP config
 local lspconfig = require("lspconfig")
-
-    lspconfig.lua_ls.setup { on_attach = on_attach }
-    lspconfig.tsserver.setup { on_attach = on_attach }
-    lspconfig.jsonls.setup { on_attach = on_attach }
-    lspconfig.rome.setup { on_attach = on_attach }
-    lspconfig.vimls.setup { on_attach = on_attach }
-    lspconfig.ltex.setup { autostart = false;
-       on_attach = on_attach }
-    lspconfig.texlab.setup { on_attach = on_attach }
+lspconfig.tsserver.setup { on_attach = on_attach }
+lspconfig.jsonls.setup { on_attach = on_attach }
+lspconfig.rome.setup { on_attach = on_attach }
+lspconfig.vimls.setup { on_attach = on_attach }
+lspconfig.ltex.setup { autostart = false; on_attach = on_attach }
+lspconfig.texlab.setup { on_attach = on_attach }
 -- lspconfig stuff    
 -- local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys
@@ -495,8 +492,39 @@ local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 -- Enable completion triggered by <c-x><c-o>
 buf_set_keymap('omnifunc', 'v:lua.vim.lsp.omnifunc')
-require'lspconfig'.sumneko_lua.setup(require("config.lua-lsp"))
-require'lspconfig'.lua_ls.setup(require("lualsp")) -- Mappings.
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
+-- require'lspconfig'.sumneko_lua.setup(require("config.lua-lsp"))
+-- require'lspconfig'.lua_ls.setup(require("lualsp")) -- Mappings.
 local opts = { noremap=true, silent=true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
