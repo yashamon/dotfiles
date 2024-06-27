f0c809f0b737d32942d4bfcdfd14eda8592e86a4
yashamon <yasha.savelyev@gmail.com>
2024-06-27 10:56:36 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 49bf49872..c895d3675 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -37,7 +37,8 @@ require("lazy").setup({    -- tag = "v2.15", -- uncomment to pin to a specific release    init = function()      -- VimTeX configuration goes here, e.g. -    vim.g.vimtex_view_method = "zathura" +    vim.g.vimtex_view_method = "", +		vim.g.vimtex_compiler_method = ''    end},  {    "ibhagwan/fzf-lua",

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -37,7 +37,8 @@ require("lazy").setup({
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "",
+		vim.g.vimtex_compiler_method = ''
   end},
 {
   "ibhagwan/fzf-lua",
