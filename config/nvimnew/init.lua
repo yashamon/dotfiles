875839b7a4ffc0dc66dc3ce2508051b4c952eb0d
yashamon <yasha.savelyev@gmail.com>
2024-06-27 10:58:25 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index c895d3675..4a64eb80e 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -37,9 +37,10 @@ require("lazy").setup({    -- tag = "v2.15", -- uncomment to pin to a specific release    init = function()      -- VimTeX configuration goes here, e.g. -    vim.g.vimtex_view_method = "", +    vim.g.vimtex_view_method = ''  		vim.g.vimtex_compiler_method = '' -  end}, +  end +},  {    "ibhagwan/fzf-lua",    -- optional for icon support

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -37,9 +37,10 @@ require("lazy").setup({
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "",
+    vim.g.vimtex_view_method = ''
 		vim.g.vimtex_compiler_method = ''
-  end},
+  end
+},
 {
   "ibhagwan/fzf-lua",
   -- optional for icon support
