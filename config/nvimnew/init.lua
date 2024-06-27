1db99b94edcb7aad7fa86a3614effeaea7deab55
yashamon <yasha.savelyev@gmail.com>
2024-06-27 16:50:38 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 47204331f..fb58ba8fa 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -33,6 +33,7 @@ require("lazy").setup({  },  {    "lervag/vimtex", +	syntax = true,    lazy = false,     -- we don't want to lazy load VimTeX    -- tag = "v2.15", -- uncomment to pin to a specific release    init = function()

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
 },
 {
   "lervag/vimtex",
+	syntax = true,
   lazy = false,     -- we don't want to lazy load VimTeX
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
