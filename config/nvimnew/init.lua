798a68023e3a71fe1a885eeb48151f83e39034ad
yashamon <yasha.savelyev@gmail.com>
2024-07-01 22:48:51 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 0ac197c03..10f0baed2 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -15,12 +15,12 @@ require("lazy").setup({    "karb94/neoscroll.nvim",    config = function ()     require('neoscroll').setup({ -  mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation -    '<C-u>', '<C-d>', -    '<C-b>', '<C-f>', -    '<C-y>', '<C-e>', -    'zt', 'zz', 'zb', -  }, +  -- mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation +  --   '<C-u>', '<C-d>', +  --   '<C-b>', '<C-f>', +  --   '<C-y>', '<C-e>', +  --   'zt', 'zz', 'zb', +  -- },    hide_cursor = true,          -- Hide cursor while scrolling    stop_eof = true,             -- Stop at <EOF> when scrolling downwards    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -15,12 +15,12 @@ require("lazy").setup({
   "karb94/neoscroll.nvim",
   config = function ()
    require('neoscroll').setup({
-  mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
-    '<C-u>', '<C-d>',
-    '<C-b>', '<C-f>',
-    '<C-y>', '<C-e>',
-    'zt', 'zz', 'zb',
-  },
+  -- mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
+  --   '<C-u>', '<C-d>',
+  --   '<C-b>', '<C-f>',
+  --   '<C-y>', '<C-e>',
+  --   'zt', 'zz', 'zb',
+  -- },
   hide_cursor = true,          -- Hide cursor while scrolling
   stop_eof = true,             -- Stop at <EOF> when scrolling downwards
   respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
