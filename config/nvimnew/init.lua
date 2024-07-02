2c7b74c06f6df39f37181b59fd2b319df5548bb1
yashamon <yasha.savelyev@gmail.com>
2024-07-01 23:00:51 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index dc470f566..fb1381c9a 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -25,22 +25,12 @@ local keymap = {    ["zt"]    = function() neoscroll.zt({ half_screen_duration = 250 }) end;    ["zz"]    = function() neoscroll.zz({ half_screen_duration = 250 }) end;    ["zb"]    = function() neoscroll.zb({ half_screen_duration = 250 }) end; -  ["G"]     = function() neoscroll.G({ half_screen_duration = 250 }) end; -  ["gg"]    = function() neoscroll.gg({ half_screen_duration = 250 }) end;  }  local modes = { 'n', 'v', 'x' }  for key, func in pairs(keymap) do    vim.keymap.set(modes, key, func) -end  hide_cursor = true,          -- Hide cursor while scrolling -  stop_eof = true,             -- Stop at <EOF> when scrolling downwards -  respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file -  cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further -  easing = 'linear',           -- Default easing function -  pre_hook = nil,              -- Function to run before the scrolling animation starts -  post_hook = nil,             -- Function to run after the scrolling animation ends -  performance_mode = false,    -- Disable "Performance Mode" on all buffers. -}) -  end +end +end  },  -- "MysticalDevil/inlay-hints.nvim",  --     event = "LspAttach",

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -25,22 +25,12 @@ local keymap = {
   ["zt"]    = function() neoscroll.zt({ half_screen_duration = 250 }) end;
   ["zz"]    = function() neoscroll.zz({ half_screen_duration = 250 }) end;
   ["zb"]    = function() neoscroll.zb({ half_screen_duration = 250 }) end;
-  ["G"]     = function() neoscroll.G({ half_screen_duration = 250 }) end;
-  ["gg"]    = function() neoscroll.gg({ half_screen_duration = 250 }) end;
 }
 local modes = { 'n', 'v', 'x' }
 for key, func in pairs(keymap) do
   vim.keymap.set(modes, key, func)
-end  hide_cursor = true,          -- Hide cursor while scrolling
-  stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-  respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  easing = 'linear',           -- Default easing function
-  pre_hook = nil,              -- Function to run before the scrolling animation starts
-  post_hook = nil,             -- Function to run after the scrolling animation ends
-  performance_mode = false,    -- Disable "Performance Mode" on all buffers.
-})
-  end
+end  
+end
 },
 -- "MysticalDevil/inlay-hints.nvim",
 --     event = "LspAttach",
