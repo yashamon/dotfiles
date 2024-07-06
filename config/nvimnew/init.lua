40bd8afc41b3dc348d74d8237a18514e74e2776c
yashamon <yasha.savelyev@gmail.com>
2024-07-06 12:06:54 -0400

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 94520edcf..60a793cb4 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -19,7 +19,7 @@ require("lazy").setup({    opts = {},    -- stylua: ignore    keys = { -    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" }, +    { "z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
   opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
