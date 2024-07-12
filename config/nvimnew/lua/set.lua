60905902ce4272fb571ef0c7cc0b77702a30b37a
yashamon <yasha.savelyev@gmail.com>
2024-07-12 14:40:09 -0400

    diff --git a/config/nvimnew/lua/set.lua b/config/nvimnew/lua/set.lua index 060ecd835..9428113d4 100644 --- a/config/nvimnew/lua/set.lua +++ b/config/nvimnew/lua/set.lua @@ -29,8 +29,8 @@ vim.o.foldlevelstart = 99  vim.o.foldenable = true
     -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
    -vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
    -vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
    +-- vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
    +-- vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
     -- opt.foldmethod='expr'
     -- opt.foldexpr='nvim_treesitter#foldexpr()'
     opt.linebreak = true

--- config/nvimnew/lua/set.lua
+++ config/nvimnew/lua/set.lua
@@ -29,8 +29,8 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+-- vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+-- vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 -- opt.foldmethod='expr'
 -- opt.foldexpr='nvim_treesitter#foldexpr()'
 opt.linebreak = true
