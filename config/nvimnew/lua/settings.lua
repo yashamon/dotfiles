0ee54b6339d8c580fa6aee4fc293b822b106e083
yashamon <yasha.savelyev@gmail.com>
2024-07-11 23:59:05 -0400

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index 693c2854c..84d4cad50 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -754,7 +754,13 @@ require("yanky").setup({    },  },  }) +vim.keymap.set({"n","x"}, ";h", "<Plug>(YankyPutAfter)") +vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)") +vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)") +vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
    +vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
    +vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
     -- C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets/all.lua
     --gitgutter
     -- require('gitsigns').setup{

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -754,7 +754,13 @@ require("yanky").setup({
   },
 },
 })
+vim.keymap.set({"n","x"}, ";h", "<Plug>(YankyPutAfter)")
+vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
+vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
+vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
 
+vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
 -- C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets/all.lua
 --gitgutter
 -- require('gitsigns').setup{
