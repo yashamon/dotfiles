d007578a4ed980b85627ae441a8719705ae3840c
yashamon <yasha.savelyev@gmail.com>
2024-10-05 08:06:05 -0600

    diff --git a/scripts/nvy.bat b/scripts/nvy.bat index cd9bbbda4..117a39460 100644 --- a/scripts/nvy.bat +++ b/scripts/nvy.bat @@ -1 +1 @@ -nu -c "C:/Users/yasha/nvy/build/nvy --fullscreen --neovim-bin=C:/Users/yasha/neovim/build/bin" +nu -c "C:/Users/yasha/nvy/build/nvy --fullscreen --neovim-bin=C:/Users/yasha/neovim/build/bin/nvim.exe"

--- scripts/nvy.bat
+++ scripts/nvy.bat
@@ -1,1 +1,1 @@
-nu -c "C:/Users/yasha/nvy/build/nvy --fullscreen --neovim-bin=C:/Users/yasha/neovim/build/bin"
+nu -c "C:/Users/yasha/nvy/build/nvy --fullscreen --neovim-bin=C:/Users/yasha/neovim/build/bin/nvim.exe"
