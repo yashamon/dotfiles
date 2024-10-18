c367c6d8317f26ebf0c05b1de4c196ce8e8d8922
yashamon <yasha.savelyev@gmail.com>
2024-10-18 11:53:38 -0600

    diff --git a/config/nvimnew/lua/functions.lua b/config/nvimnew/lua/functions.lua index 4f425bd78..2e23e1e28 100644 --- a/config/nvimnew/lua/functions.lua +++ b/config/nvimnew/lua/functions.lua @@ -110,7 +110,7 @@ end
     GitAsync = function()
     -- local buff = vim.fn.bufname()
    -local command1 = 'if ((git rev-parse --is-inside-work-tree) | into bool) {git add .; git diff --staged | save --force message.log; git commit -F message.log; git push --all origin; echo "synced"}'
    +local command1 = 'if ((git rev-parse --is-inside-work-tree) | into bool) do -i {git add .}; do -i {git diff --staged | save --force message.log}; do -i {git commit -F message.log}; do -i {git push --all origin}; do -i {echo "synced"}'
     Job(command1)
     end
     -- test3

--- config/nvimnew/lua/functions.lua
+++ config/nvimnew/lua/functions.lua
@@ -110,7 +110,7 @@ end
 
 GitAsync = function()
 -- local buff = vim.fn.bufname()
-local command1 = 'if ((git rev-parse --is-inside-work-tree) | into bool) {git add .; git diff --staged | save --force message.log; git commit -F message.log; git push --all origin; echo "synced"}'
+local command1 = 'if ((git rev-parse --is-inside-work-tree) | into bool) do -i {git add .}; do -i {git diff --staged | save --force message.log}; do -i {git commit -F message.log}; do -i {git push --all origin}; do -i {echo "synced"}'
 Job(command1)
 end
 -- test3
