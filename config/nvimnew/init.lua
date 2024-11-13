c51a64c472bbb54ec9716dae83582881a656266f
yashamon <yasha.savelyev@gmail.com>
2024-11-13 13:23:54 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 34106c785..060ae2139 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -15,6 +15,100 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then                                              end  vim.opt.rtp:append(lazypath)  require("lazy").setup({ +{ +  'tim-harding/neophyte', +  tag = '0.3.0', +  event = 'VeryLazy', +  opts = { +    -- Same as neophyte.setup({ ... }) +  }, +} + +-- API usage example: +local neophyte = require('neophyte') +neophyte.setup({ +  fonts = { +    { +      name = 'Cascadia Code PL', +      features = { +        { +          name = 'calt', +          value = 1, +        }, +        -- Shorthand to set a feature to 1 +        'ss01', +        'ss02', +      }, +    }, +    -- Fallback fonts +    { +      name = 'Monaspace Argon Var', +      -- Variable font axes +      variations = { +        { +          name = 'slnt', +          value = -11, +        }, +      }, +    }, +    -- Shorthand for no features or variations +    'Symbols Nerd Font', +    'Noto Color Emoji', +  }, +  font_size = { +    kind = 'width', -- 'width' | 'height' +    size = 10, +  }, +  -- Multipliers of the base animation speed. +  -- To disable animations, set these to large values like 1000. +  cursor_speed = 2, +  scroll_speed = 2, +  -- Increase or decrease the distance from the baseline for underlines. +  underline_offset = 1, +  -- For transparent window effects, use this to set the default background color. +  -- This is because most colorschemes in transparent mode unset the background, +  -- which normally defaults to the terminal background, but we don't have that here. +  -- You must also pass --transparent as a command-line argument to see the effect. +  -- Channel values are in the range 0-255. +  bg_override = { +    r = 48, +    g = 52, +    b = 70, +    a = 128, +  }, +}) + +-- Alternatively, the guifont option is supported: +vim.opt.guifont = 'Cascadia Code PL:w10, Symbols Nerd Font, Noto Color Emoji' + +-- There are also freestanding functions to set these options as desired: + +-- Increase font size +vim.keymap.set('n', '<c-+>', function() +  neophyte.set_font_width(neophyte.get_font_width() + 1) +end) + +-- Decrease font size +vim.keymap.set('n', '<c-->', function() +  neophyte.set_font_width(neophyte.get_font_width() - 1) +end) + +-- Use cmd-ctrl-f to toggle fullsreen on MacOS +if this_os:find('Darwin') then +  vim.keymap.set('n', '<DC-f>', function() +    neophyte.set_fullscreen(not neophyte.get_fullscreen()) +  end) +end + +-- Neophyte can also record frames to a PNG sequence. +-- You can convert to a video with ffmpeg: +-- +-- ffmpeg -framerate 60 -pattern_type glob -i '/my/frames/location/*.png' +-- -pix_fmt yuv420p -c:v libx264 -vf +-- "colorspace=all=bt709:iprimaries=bt709:itrc=srgb:ispace=bt709:range=tv:irange=pc" +-- -color_range 1 -colorspace 1 -color_primaries 1 -crf 23 -y /my/output/video.mp4 +neophyte.start_render('/directory/to/output/frames/') +neophyte.end_render()  -- { 'stonelasley/flare.nvim' },  -- {  --   "folke/flash.nvim", diff --git a/config/nvimnew/spell/en.utf-8.add b/config/nvimnew/spell/en.utf-8.add index 5ed6b4282..2948273aa 100644 --- a/config/nvimnew/spell/en.utf-8.add +++ b/config/nvimnew/spell/en.utf-8.add @@ -1000,3 +1000,5 @@ enumerability  formalizable  complexification  Langlands +Iritani +Givental diff --git a/config/nvimnew/spell/en.utf-8.add.spl b/config/nvimnew/spell/en.utf-8.add.spl index 59fcb683b..fe211f800 100644 Binary files a/config/nvimnew/spell/en.utf-8.add.spl and b/config/nvimnew/spell/en.utf-8.add.spl differ

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -15,6 +15,100 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
                                             end
 vim.opt.rtp:append(lazypath)
 require("lazy").setup({
+{
+  'tim-harding/neophyte',
+  tag = '0.3.0',
+  event = 'VeryLazy',
+  opts = {
+    -- Same as neophyte.setup({ ... })
+  },
+}
+
+-- API usage example:
+local neophyte = require('neophyte')
+neophyte.setup({
+  fonts = {
+    {
+      name = 'Cascadia Code PL',
+      features = {
+        {
+          name = 'calt',
+          value = 1,
+        },
+        -- Shorthand to set a feature to 1
+        'ss01',
+        'ss02',
+      },
+    },
+    -- Fallback fonts
+    {
+      name = 'Monaspace Argon Var',
+      -- Variable font axes
+      variations = {
+        {
+          name = 'slnt',
+          value = -11,
+        },
+      },
+    },
+    -- Shorthand for no features or variations
+    'Symbols Nerd Font',
+    'Noto Color Emoji',
+  },
+  font_size = {
+    kind = 'width', -- 'width' | 'height'
+    size = 10,
+  },
+  -- Multipliers of the base animation speed.
+  -- To disable animations, set these to large values like 1000.
+  cursor_speed = 2,
+  scroll_speed = 2,
+  -- Increase or decrease the distance from the baseline for underlines.
+  underline_offset = 1,
+  -- For transparent window effects, use this to set the default background color.
+  -- This is because most colorschemes in transparent mode unset the background,
+  -- which normally defaults to the terminal background, but we don't have that here.
+  -- You must also pass --transparent as a command-line argument to see the effect.
+  -- Channel values are in the range 0-255.
+  bg_override = {
+    r = 48,
+    g = 52,
+    b = 70,
+    a = 128,
+  },
+})
+
+-- Alternatively, the guifont option is supported:
+vim.opt.guifont = 'Cascadia Code PL:w10, Symbols Nerd Font, Noto Color Emoji'
+
+-- There are also freestanding functions to set these options as desired:
+
+-- Increase font size
+vim.keymap.set('n', '<c-+>', function()
+  neophyte.set_font_width(neophyte.get_font_width() + 1)
+end)
+
+-- Decrease font size
+vim.keymap.set('n', '<c-->', function()
+  neophyte.set_font_width(neophyte.get_font_width() - 1)
+end)
+
+-- Use cmd-ctrl-f to toggle fullsreen on MacOS
+if this_os:find('Darwin') then
+  vim.keymap.set('n', '<DC-f>', function()
+    neophyte.set_fullscreen(not neophyte.get_fullscreen())
+  end)
+end
+
+-- Neophyte can also record frames to a PNG sequence.
+-- You can convert to a video with ffmpeg:
+--
+-- ffmpeg -framerate 60 -pattern_type glob -i '/my/frames/location/*.png'
+-- -pix_fmt yuv420p -c:v libx264 -vf
+-- "colorspace=all=bt709:iprimaries=bt709:itrc=srgb:ispace=bt709:range=tv:irange=pc"
+-- -color_range 1 -colorspace 1 -color_primaries 1 -crf 23 -y /my/output/video.mp4
+neophyte.start_render('/directory/to/output/frames/')
+neophyte.end_render()
 -- { 'stonelasley/flare.nvim' },
 -- {
 --   "folke/flash.nvim",
