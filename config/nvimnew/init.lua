7930a09fe9b56d85b661adecd97f3fbcac57f7b0
yashamon <yasha.savelyev@gmail.com>
2024-11-13 20:17:16 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 672361cef..40577fd6f 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -15,6 +15,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then                                              end  vim.opt.rtp:append(lazypath)  require("lazy").setup({ +{ +    "lmburns/lf.nvim", +    config = function() +        -- This feature will not work if the plugin is lazy-loaded +        vim.g.lf_netrw = 1 + +        require("lf").setup({ +            escape_quit = false, +            border = "rounded", +        }) + +        vim.keymap.set("n", "<M-o>", "<Cmd>Lf<CR>") + +        vim.api.nvim_create_autocmd({ +            event = "User", +            pattern = "LfTermEnter", +            callback = function(a) +                vim.api.nvim_buf_set_keymap(a.buf, "t", "q", "q", {nowait = true}) +            end, +        }) +    end, +    requires = {"toggleterm.nvim"} +},  -- {  --   'tim-harding/neophyte',  --   tag = '0.3.0',

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -15,6 +15,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
                                             end
 vim.opt.rtp:append(lazypath)
 require("lazy").setup({
+{
+    "lmburns/lf.nvim",
+    config = function()
+        -- This feature will not work if the plugin is lazy-loaded
+        vim.g.lf_netrw = 1
+
+        require("lf").setup({
+            escape_quit = false,
+            border = "rounded",
+        })
+
+        vim.keymap.set("n", "<M-o>", "<Cmd>Lf<CR>")
+
+        vim.api.nvim_create_autocmd({
+            event = "User",
+            pattern = "LfTermEnter",
+            callback = function(a)
+                vim.api.nvim_buf_set_keymap(a.buf, "t", "q", "q", {nowait = true})
+            end,
+        })
+    end,
+    requires = {"toggleterm.nvim"}
+},
 -- {
 --   'tim-harding/neophyte',
 --   tag = '0.3.0',
