772d7464e6e4fb319a5482bc096bdbde0fa7347b
yashamon <yasha.savelyev@gmail.com>
2025-09-16 12:00:15 -0600

    -a

--- config/nvimnew/lua/snippets/all.lua
+++ config/nvimnew/lua/snippets/all.lua
@@ -505,6 +505,15 @@ ls.add_snippets("tex", {
 	}, { condition = Is_math }),
 })
 
+ls.add_snippets("tex", {
+	-- rec_ls is self-referencing. That makes this snippet 'infinite' eg. have as many
+	-- \item as necessary by utilizing a choiceNode.
+	s("bul", {
+		t({"\\bullet "}), i("1",""),
+	}, { condition = Is_math }),
+})
+
+
 -- "overline": {
 --     "prefix": ["over"],
 --     "body": [
