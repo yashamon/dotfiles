dde7289b1be895f7c8f83917b4ee4219a02ec25d
yashamon <yasha.savelyev@gmail.com>
2026-01-19 23:19:07 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index a0b679aaa..963e07996 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -22,6 +22,7 @@ require("lazy").setup({    dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },    opts = {      snippets = { preset = 'luasnip' }, +    keymap = { preset = "super-tab" },      -- ensure you have the `snippets` source (enabled by default)      sources = {        default = { 'lsp', 'path', 'snippets', 'buffer' },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
   dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
   opts = {
     snippets = { preset = 'luasnip' },
+    keymap = { preset = "super-tab" },
     -- ensure you have the `snippets` source (enabled by default)
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
