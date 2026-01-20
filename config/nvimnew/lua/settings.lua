1356cfab6f1b40c783328826158b74a426b173a7
yashamon <yasha.savelyev@gmail.com>
2026-01-19 23:13:26 -0600

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index 83ca2a4c3..7723b5a8d 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -550,20 +550,20 @@ vim.lsp.enable({"vimls"})  --       "fallback",  --     },  --   }, - -{ -  'saghen/blink.cmp', -  version = '1.*', -  -- `main` is untested, please open a PR if you've confirmed it works as expected -  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' }, -  opts = { -    snippets = { preset = 'luasnip' }, -    -- ensure you have the `snippets` source (enabled by default) -    sources = { -      default = { 'lsp', 'path', 'snippets', 'buffer' }, -    }, -  } -}}) +-- +-- { +--   'saghen/blink.cmp', +--   version = '1.*', +--   -- `main` is untested, please open a PR if you've confirmed it works as expected +--   dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' }, +--   opts = { +--     snippets = { preset = 'luasnip' }, +--     -- ensure you have the `snippets` source (enabled by default) +--     sources = { +--       default = { 'lsp', 'path', 'snippets', 'buffer' }, +--     }, +--   } +-- }})
     -- local has_words_before = function()

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -550,20 +550,20 @@ vim.lsp.enable({"vimls"})
 --       "fallback",
 --     },
 --   },
-
-{
-  'saghen/blink.cmp',
-  version = '1.*',
-  -- `main` is untested, please open a PR if you've confirmed it works as expected
-  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-  opts = {
-    snippets = { preset = 'luasnip' },
-    -- ensure you have the `snippets` source (enabled by default)
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-  }
-}})
+--
+-- {
+--   'saghen/blink.cmp',
+--   version = '1.*',
+--   -- `main` is untested, please open a PR if you've confirmed it works as expected
+--   dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+--   opts = {
+--     snippets = { preset = 'luasnip' },
+--     -- ensure you have the `snippets` source (enabled by default)
+--     sources = {
+--       default = { 'lsp', 'path', 'snippets', 'buffer' },
+--     },
+--   }
+-- }})
 
 
 -- local has_words_before = function()
