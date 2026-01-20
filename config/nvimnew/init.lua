eda602805baf8e9bd622667647a1af8ba7733b10
yashamon <yasha.savelyev@gmail.com>
2026-01-20 13:03:32 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index e6f932e60..b96911d8e 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -265,13 +265,13 @@ function Is_math()      return vim.api.nvim_eval('vimtex#syntax#in_mathzone()') == 1  end
    -require("luasnip/loaders/from_vscode").load({
    -	paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",
    -  -- fs_event_providers = {
    -                    -- autocmd = true,
    -                    -- libuv = true,
    -	-- },
    -})
    +-- require("luasnip/loaders/from_vscode").load({
    +-- 	paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",
    +--   -- fs_event_providers = {
    +--                     -- autocmd = true,
    +--                     -- libuv = true,
    +-- 	-- },
    +-- })
     -- Reload = function()
     require("luasnip.loaders.from_lua").load({
                     paths = "C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets",

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -265,13 +265,13 @@ function Is_math()
     return vim.api.nvim_eval('vimtex#syntax#in_mathzone()') == 1
 end
 
-require("luasnip/loaders/from_vscode").load({
-	paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",
-  -- fs_event_providers = {
-                    -- autocmd = true,
-                    -- libuv = true,
-	-- },
-})
+-- require("luasnip/loaders/from_vscode").load({
+-- 	paths = "C:/Users/yasha/OneDrive/dotfiles/snippets",
+--   -- fs_event_providers = {
+--                     -- autocmd = true,
+--                     -- libuv = true,
+-- 	-- },
+-- })
 -- Reload = function()
 require("luasnip.loaders.from_lua").load({
                 paths = "C:/Users/yasha/OneDrive/dotfiles/config/nvimnew/lua/snippets",
