98e3d47ccab66e02ca04ba6dc12d11c7625af2a3
yashamon <yasha.savelyev@gmail.com>
2026-01-21 08:54:31 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index f965efe84..bfc2ef31c 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -19,7 +19,9 @@ require("lazy").setup({    'saghen/blink.cmp',    version = '1.*',    -- `main` is untested, please open a PR if you've confirmed it works as expected -  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' }, +  dependencies = { +			{'L3MON4D3/LuaSnip', version = 'v2.*'} +		},    opts = {      snippets = { preset = 'luasnip', score_offset = 40000 },      keymap = { preset = "super-tab", ["<m-k>"] = { "select_prev", "snippet_backward", "fallback" },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -19,7 +19,9 @@ require("lazy").setup({
   'saghen/blink.cmp',
   version = '1.*',
   -- `main` is untested, please open a PR if you've confirmed it works as expected
-  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+  dependencies = {
+			{'L3MON4D3/LuaSnip', version = 'v2.*'}
+		},
   opts = {
     snippets = { preset = 'luasnip', score_offset = 40000 },
     keymap = { preset = "super-tab", ["<m-k>"] = { "select_prev", "snippet_backward", "fallback" },
