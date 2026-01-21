155ba334ed2751ed7f09a127740b94c5e9add9ba
yashamon <yasha.savelyev@gmail.com>
2026-01-21 08:54:42 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index bfc2ef31c..dd45762b3 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -20,7 +20,11 @@ require("lazy").setup({    version = '1.*',    -- `main` is untested, please open a PR if you've confirmed it works as expected    dependencies = { -			{'L3MON4D3/LuaSnip', version = 'v2.*'} +			{'L3MON4D3/LuaSnip', version = 'v2.*'}, +			{ +            'Kaiser-Yang/blink-cmp-dictionary', +            dependencies = { 'nvim-lua/plenary.nvim' } +        }  		},    opts = {      snippets = { preset = 'luasnip', score_offset = 40000 },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -20,7 +20,11 @@ require("lazy").setup({
   version = '1.*',
   -- `main` is untested, please open a PR if you've confirmed it works as expected
   dependencies = {
-			{'L3MON4D3/LuaSnip', version = 'v2.*'}
+			{'L3MON4D3/LuaSnip', version = 'v2.*'},
+			{
+            'Kaiser-Yang/blink-cmp-dictionary',
+            dependencies = { 'nvim-lua/plenary.nvim' }
+        }
 		},
   opts = {
     snippets = { preset = 'luasnip', score_offset = 40000 },
