fb2b95a90c4e100fd07ce3bc45ba3c26eebf784e
yashamon <yasha.savelyev@gmail.com>
2026-01-20 21:54:59 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index adcd0766b..40df498e4 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -27,7 +27,7 @@ require("lazy").setup({  			},      -- ensure you have the `snippets` source (enabled by default)      sources = {min_keyword_length = 0, -      default = {'snippets', 'lsp', 'buffer', 'path' }, +      default = {'lsp', 'buffer', 'path' },      },    }  },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
 			},
     -- ensure you have the `snippets` source (enabled by default)
     sources = {min_keyword_length = 0,
-      default = {'snippets', 'lsp', 'buffer', 'path' },
+      default = {'lsp', 'buffer', 'path' },
     },
   }
 },
