e008c0165b646c5a8fbb7b3cd3d315c8b2448568
yashamon <yasha.savelyev@gmail.com>
2026-01-21 09:23:17 -0600

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index dcb4e1a94..0adf1a28c 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -383,7 +383,7 @@ require("mason").setup()  -- local lspconfig = require("lspconfig")  -- lazy.nvim  vim.lsp.config("ctags_lsp", { -			cmd = { "C:/Users/yasha/go/ctags_lsp" }, +			cmd = { "C:/Users/yasha/go/ctags-lsp" },  			filetypes = { "latex", "tex" },  			root_dir = vim.uv.cwd(),  		})

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -383,7 +383,7 @@ require("mason").setup()
 -- local lspconfig = require("lspconfig")
 -- lazy.nvim
 vim.lsp.config("ctags_lsp", {
-			cmd = { "C:/Users/yasha/go/ctags_lsp" },
+			cmd = { "C:/Users/yasha/go/ctags-lsp" },
 			filetypes = { "latex", "tex" },
 			root_dir = vim.uv.cwd(),
 		})
