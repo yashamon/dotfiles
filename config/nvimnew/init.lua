7a5c2cd862bce489339bce0988ca4e7d9b48fafb
yashamon <yasha.savelyev@gmail.com>
2026-01-21 20:13:15 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index d41e695b0..86606ecd2 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -49,6 +49,27 @@ require("lazy").setup({                          -- options for blink-cmp-dictionary                      }                  }, +					buffer = { +      name = 'Buffer', +      module = 'blink.cmp.sources.buffer', +      opts = {}, -- Passed to the source directly, varies by source + +      --- NOTE: All of these options may be functions to get dynamic behavior +      --- See the type definitions for more information +      enabled = true, -- Whether or not to enable the provider +      async = false, -- Whether we should show the completions before this provider returns, without waiting for it +      timeout_ms = 2000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous +      transform_items = nil, -- Function to transform the items before they're returned +      should_show_items = true, -- Whether or not to show the items +      max_items = nil, -- Maximum number of items to display in the menu +      min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider +      -- If this provider returns 0 items, it will fallback to these providers. +      -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback +      fallbacks = {}, +      score_offset = 0, -- Boost/penalize the score of the items +      override = nil, -- Override the source's functions +    } +  									}          }    }

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -49,6 +49,27 @@ require("lazy").setup({
                         -- options for blink-cmp-dictionary
                     }
                 },
+					buffer = {
+      name = 'Buffer',
+      module = 'blink.cmp.sources.buffer',
+      opts = {}, -- Passed to the source directly, varies by source
+    
+      --- NOTE: All of these options may be functions to get dynamic behavior
+      --- See the type definitions for more information
+      enabled = true, -- Whether or not to enable the provider
+      async = false, -- Whether we should show the completions before this provider returns, without waiting for it
+      timeout_ms = 2000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
+      transform_items = nil, -- Function to transform the items before they're returned
+      should_show_items = true, -- Whether or not to show the items
+      max_items = nil, -- Maximum number of items to display in the menu
+      min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
+      -- If this provider returns 0 items, it will fallback to these providers.
+      -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
+      fallbacks = {},
+      score_offset = 0, -- Boost/penalize the score of the items
+      override = nil, -- Override the source's functions
+    }
+
 									}
         }
   }
