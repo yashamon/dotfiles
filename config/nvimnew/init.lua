99178995fb0635314fbffb7c9d6a9d780070a015
yashamon <yasha.savelyev@gmail.com>
2026-01-21 20:21:15 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 2b269ac66..701e7fda0 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -30,7 +30,7 @@ require("lazy").setup({      },  		},    opts = { -    snippets = { preset = 'luasnip', score_offset = 40000 }, +    snippets = { preset = 'luasnip', score_offset = 40000, async = true },      keymap = { preset = "super-tab", ["<m-k>"] = { "snippet_backward", "select_prev", "fallback" },  				["<m-j>"] = { "snippet_forward", "select_next",  "fallback" },  			},

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
     },
 		},
   opts = {
-    snippets = { preset = 'luasnip', score_offset = 40000 },
+    snippets = { preset = 'luasnip', score_offset = 40000, async = true },
     keymap = { preset = "super-tab", ["<m-k>"] = { "snippet_backward", "select_prev", "fallback" },
 				["<m-j>"] = { "snippet_forward", "select_next",  "fallback" },
 			},
