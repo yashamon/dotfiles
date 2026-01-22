1edd711f56946f434047c9b254cba021077ecd44
yashamon <yasha.savelyev@gmail.com>
2026-01-21 20:09:48 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index bc33c261b..d41e695b0 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -37,7 +37,7 @@ require("lazy").setup({      -- ensure you have the `snippets` source (enabled by default)      sources = {              -- Add 'dictionary' to the list -            default = {{'dictionary', score_offset = 30000}, 'lsp', 'path', 'buffer', 'snippets' }, +            default = {'buffer', 'lsp', 'path', 'snippets' },              providers = {                  dictionary = {                      module = 'blink-cmp-dictionary',

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
     -- ensure you have the `snippets` source (enabled by default)
     sources = {
             -- Add 'dictionary' to the list
-            default = {{'dictionary', score_offset = 30000}, 'lsp', 'path', 'buffer', 'snippets' },
+            default = {'buffer', 'lsp', 'path', 'snippets' },
             providers = {
                 dictionary = {
                     module = 'blink-cmp-dictionary',
