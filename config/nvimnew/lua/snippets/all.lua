cd731a35b908003077ecb986d227b02703753acf
yashamon <yasha.savelyev@gmail.com>
2026-03-03 17:20:48 -0600

    diff --git a/config/nvimnew/lua/snippets/all.lua b/config/nvimnew/lua/snippets/all.lua index 433345002..b9dde8e7c 100644 --- a/config/nvimnew/lua/snippets/all.lua +++ b/config/nvimnew/lua/snippets/all.lua @@ -560,6 +560,15 @@ ls.add_snippets("tex", {  		t({"\\leq "}), i(1, ""),  	}, { condition = Is_math }),  }) +ls.add_snippets("tex", { +	-- rec_ls is self-referencing. That makes this snippet 'infinite' eg. have as many +	-- \item as necessary by utilizing a choiceNode. +	s("cite", { +		t({"\\cite{"}), i(1, ""), t({"}"}), +			i("1",""), +	}, { condition = Is_math }), +}) +  ls.add_snippets("tex", {  	-- rec_ls is self-referencing. That makes this snippet 'infinite' eg. have as many  	-- \item as necessary by utilizing a choiceNode.

--- config/nvimnew/lua/snippets/all.lua
+++ config/nvimnew/lua/snippets/all.lua
@@ -563,6 +563,15 @@ ls.add_snippets("tex", {
 ls.add_snippets("tex", {
 	-- rec_ls is self-referencing. That makes this snippet 'infinite' eg. have as many
 	-- \item as necessary by utilizing a choiceNode.
+	s("cite", {
+		t({"\\cite{"}), i(1, ""), t({"}"}),
+			i("1",""),
+	}, { condition = Is_math }),
+})
+
+ls.add_snippets("tex", {
+	-- rec_ls is self-referencing. That makes this snippet 'infinite' eg. have as many
+	-- \item as necessary by utilizing a choiceNode.
 	s("dne", {
 		t({"\\nexists "}), i(1, ""), 
 		i("1",""),
