8d71716298b16bab302a5c690a73533f358a9f58
yashamon <yasha.savelyev@gmail.com>
2026-03-03 22:37:11 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 376d72510..9acdc890e 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -48,15 +48,16 @@ require("lazy").setup({      snippets = { preset = 'luasnip', score_offset = 0},  keymap = {    preset = "none", +    ["<Tab>"] = { -    "snippet_forward",   -- expand or jump forward in LuaSnip -    "select_next",       -- cycle completion items -    "fallback",          -- literal tab +    "snippet_forward", +    "select_next_or_first", +    "fallback",    },
       ["<S-Tab>"] = {
    -    "snippet_backward",  -- jump backward in LuaSnip
    -    "select_prev",       -- cycle backward
    +    "snippet_backward",
    +    "select_prev",
         "fallback",
       },
     },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -48,15 +48,16 @@ require("lazy").setup({
     snippets = { preset = 'luasnip', score_offset = 0},
 keymap = {
   preset = "none",
+
   ["<Tab>"] = {
-    "snippet_forward",   -- expand or jump forward in LuaSnip
-    "select_next",       -- cycle completion items
-    "fallback",          -- literal tab
+    "snippet_forward",
+    "select_next_or_first",
+    "fallback",
   },
 
   ["<S-Tab>"] = {
-    "snippet_backward",  -- jump backward in LuaSnip
-    "select_prev",       -- cycle backward
+    "snippet_backward",
+    "select_prev",
     "fallback",
   },
 },
