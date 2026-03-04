b25b5a98ca42cf87f17ba462ed9a11e3db832fd0
yashamon <yasha.savelyev@gmail.com>
2026-03-03 21:25:38 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index ee4272a3f..67efb47da 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -46,9 +46,9 @@ require("lazy").setup({  		},    opts = {      snippets = { preset = 'luasnip', score_offset = 0}, -			--  keymap = { preset = "super-tab", ["<m-k>"] = { "snippet_backward", "select_prev", "fallback" }, -			-- 	["<m-j>"] = { "snippet_forward", "select_next",  "fallback" }, -			-- }, +			 keymap = { preset = "super-tab", ["<m-k>"] = { "snippet_backward", "select_prev", "fallback" }, +				["<m-j>"] = { "snippet_forward", "select_next",  "fallback" }, +			},      -- ensure you have the `snippets` source (enabled by default)      sources = {              -- Add 'dictionary' to the list @@ -249,7 +249,7 @@ require("lazy").setup({        },      })    end, -} +},
     {
       'stevearc/oil.nvim',

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -46,9 +46,9 @@ require("lazy").setup({
 		},
   opts = {
     snippets = { preset = 'luasnip', score_offset = 0},
-			--  keymap = { preset = "super-tab", ["<m-k>"] = { "snippet_backward", "select_prev", "fallback" },
-			-- 	["<m-j>"] = { "snippet_forward", "select_next",  "fallback" },
-			-- },
+			 keymap = { preset = "super-tab", ["<m-k>"] = { "snippet_backward", "select_prev", "fallback" },
+				["<m-j>"] = { "snippet_forward", "select_next",  "fallback" },
+			},
     -- ensure you have the `snippets` source (enabled by default)
     sources = {
             -- Add 'dictionary' to the list
@@ -249,7 +249,7 @@ require("lazy").setup({
       },
     })
   end,
-}
+},
 
 {
   'stevearc/oil.nvim',
