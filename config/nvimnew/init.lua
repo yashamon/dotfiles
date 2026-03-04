0dc202a610d17b3b84f16830a89cf792588afbe2
yashamon <yasha.savelyev@gmail.com>
2026-03-03 22:49:11 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index bae0db177..4abe2d7ba 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -59,7 +59,7 @@ keymap = {      "select_prev",       -- cycle backward      "fallback",    }, -["<CR>"] = "accept" +["<CR>"] = {"accept"}  },       -- ensure you have the `snippets` source (enabled by default)      sources = {

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -59,7 +59,7 @@ keymap = {
     "select_prev",       -- cycle backward
     "fallback",
   },
-["<CR>"] = "accept"
+["<CR>"] = {"accept"}
 },
      -- ensure you have the `snippets` source (enabled by default)
     sources = {
