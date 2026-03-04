b662e7c50b481980c3a34a75147a9de6a395998b
yashamon <yasha.savelyev@gmail.com>
2026-03-03 22:36:41 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 9acdc890e..79513a3cc 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -50,14 +50,14 @@ keymap = {    preset = "none",
       ["<Tab>"] = {
    -    "snippet_forward",
    -    "select_next_or_first",
    -    "fallback",
    +    "snippet_forward",   -- expand or jump forward in LuaSnip
    +    "select_next",       -- cycle completion items
    +    "fallback",          -- literal tab
       },
    
       ["<S-Tab>"] = {
    -    "snippet_backward",
    -    "select_prev",
    +    "snippet_backward",  -- jump backward in LuaSnip
    +    "select_prev",       -- cycle backward
         "fallback",
       },
     },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -50,14 +50,14 @@ keymap = {
   preset = "none",
 
   ["<Tab>"] = {
-    "snippet_forward",
-    "select_next_or_first",
-    "fallback",
+    "snippet_forward",   -- expand or jump forward in LuaSnip
+    "select_next",       -- cycle completion items
+    "fallback",          -- literal tab
   },
 
   ["<S-Tab>"] = {
-    "snippet_backward",
-    "select_prev",
+    "snippet_backward",  -- jump backward in LuaSnip
+    "select_prev",       -- cycle backward
     "fallback",
   },
 },
