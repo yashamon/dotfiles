5ddaf311148bc56f3dff9bddf0c509d7be9ffd5a
yashamon <yasha.savelyev@gmail.com>
2026-03-03 22:05:14 -0600

    diff --git a/config/nvimnew/init.lua b/config/nvimnew/init.lua index 2f9524ae5..d7e16f5c7 100644 --- a/config/nvimnew/init.lua +++ b/config/nvimnew/init.lua @@ -46,12 +46,7 @@ require("lazy").setup({  		},    opts = {      snippets = { preset = 'luasnip', score_offset = 0}, -			 keymap = { -    preset = "default", -    ["<Tab>"] = false, -    ["<S-Tab>"] = false, -  }, -    -- ensure you have the `snippets` source (enabled by default) +			     -- ensure you have the `snippets` source (enabled by default)      sources = {              -- Add 'dictionary' to the list              default = {'buffer', 'lsp', 'path' },

--- config/nvimnew/init.lua
+++ config/nvimnew/init.lua
@@ -46,12 +46,7 @@ require("lazy").setup({
 		},
   opts = {
     snippets = { preset = 'luasnip', score_offset = 0},
-			 keymap = {
-    preset = "default",
-    ["<Tab>"] = false,
-    ["<S-Tab>"] = false,
-  },
-    -- ensure you have the `snippets` source (enabled by default)
+			     -- ensure you have the `snippets` source (enabled by default)
     sources = {
             -- Add 'dictionary' to the list
             default = {'buffer', 'lsp', 'path' },
