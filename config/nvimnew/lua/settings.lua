03d389e5d9a63953002154be453543a465d3668e
yashamon <yasha.savelyev@gmail.com>
2026-03-07 20:36:20 -0600

    diff --git a/config/nvimnew/lua/settings.lua b/config/nvimnew/lua/settings.lua index e8220e84a..60635e91e 100644 --- a/config/nvimnew/lua/settings.lua +++ b/config/nvimnew/lua/settings.lua @@ -498,12 +498,17 @@ vim.keymap.set("i", "<Tab>", function()      ls.expand_or_jump()    end  end, { silent = true }) - -vim.keymap.set("i", "<S-Tab>", function() +vim.keymap.set("i", "<m-l>", function() +  if ls.jumpable() then +    ls.jump() +  end +end, { silent = true }) +vim.keymap.set("i", "<m-h", function()    if ls.jumpable(-1) then      ls.jump(-1)    end  end, { silent = true }) +  require("fzf-lua").setup({    profiles = {      default = {},   -- override default profile

--- config/nvimnew/lua/settings.lua
+++ config/nvimnew/lua/settings.lua
@@ -498,12 +498,17 @@ vim.keymap.set("i", "<Tab>", function()
     ls.expand_or_jump()
   end
 end, { silent = true })
-
-vim.keymap.set("i", "<S-Tab>", function()
+vim.keymap.set("i", "<m-l>", function()
+  if ls.jumpable() then
+    ls.jump()
+  end
+end, { silent = true })
+vim.keymap.set("i", "<m-h", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
 end, { silent = true })
+
 require("fzf-lua").setup({
   profiles = {
     default = {},   -- override default profile
